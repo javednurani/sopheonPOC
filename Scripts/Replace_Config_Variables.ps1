8d6283646d9a1f31b7970e3ad8c6c2154b343687
Cal Hueber <Cal.Hueber@sopheon.com>
2021-07-01 11:22:36 -0600

    Added replace script for Index.html for dynamic url

---
+++ Scripts/Replace_Config_Variables.ps1
@@ -0,0 +1,18 @@
+[Cmdletbinding()]
+Param(
+    [Parameter(Mandatory = $true)][string]$PathToFile
+)
+
+#Environment should always be set from the Pipeline variables. If this isn't set, nothing is getting out
+$Environment = $env:Environment;
+
+$TenantName = (az keyvault secret show --vault-name "Stratus-$($Environment)" --name "StratusB2CTenantName" --query value).Replace('"', '');
+$JWTClientId = (az keyvault secret show --vault-name "Stratus-$($Environment)" --name "StratusB2CJWTClientId" --query value).Replace('"', '');
+
+$fileContent = Get-Content $PathToFile;
+
+$fileContent = $fileContent.Replace("non-existent.onmicrosoft.com", $TenantName);
+$fileContent = $fileContent.Replace("^B2CLoginName^", $TenantName.Replace(".onmicrosoft.com". ""));
+$fileContent = $fileContent.Replace("^B2CClientId^", $JWTClientId);
+
+Set-Content -Path $PathToFile -Value $fileContent -Force;
