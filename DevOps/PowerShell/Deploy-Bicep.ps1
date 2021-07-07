a53db38ab9bd2b1d04ec77329b67eb3a0b8ca6da
Cal Hueber <Cal.Hueber@sopheon.com>
2021-07-07 09:14:34 -0600

    Updated master deploy with current azure resource needs

--- DevOps/PowerShell/Deploy-Bicep.ps1
+++ DevOps/PowerShell/Deploy-Bicep.ps1
@@ -5,26 +5,18 @@ $DeploymentName = "ADO-Deployment";
 $MasterTemplate = "$($PSScriptRoot)\..\Bicep\Master_Template.bicep";
 $MasterParametersTemplate = "$($PSScriptRoot)\..\Bicep\Master_Template_Parameters.json";
 
-$CDNTemplate = "$($PSScriptRoot)\..\Bicep\CDN_Template.bicep";
-$CDNParametersTemplate = "$($PSScriptRoot)\..\Bicep\CDN_Template_Parameters.json";
-
 #region Master template
 Write-Host "Replacing tokens on Master Template...";
 $a = Get-Content $MasterTemplate -raw;
-$a = $a -replace $StorageAccountNameToken, $StorageAccountNameValue -replace $ResourceGroupToken, $ResourceGroupValue;
-$a = $a -replace $AppServiceNameToken, $AppServiceNameValue -replace $ResourceGroupToken, $ResourceGroupValue;
-$a = $a -replace $SqlServerNameToken, $SqlServerNameValue -replace $SqlServerElasticPoolToken, $SqlServerElasticPoolValue;
-$a = $a -replace $SqlServerDatabaseNameToken, $SqlServerDatabaseNameValue -replace $AppInsightsSpaNameToken, $AppInsightsSpaNameValue;
+$a = $a -replace $StorageAccountNameToken, $StorageAccountNameValue -replace $KeyVaultNameToken, $KeyVaultNameValue
+$a = $a -replace $ResourceGroupToken, $ResourceGroupValue;
 Set-Content -Value $a -Path $MasterTemplate;
 Write-Host "Complete!";
 
 Write-Host "Replacing tokens on Master Parameters Template...";
 $a = Get-Content $MasterParametersTemplate -raw;
 $a = $a -replace $StorageAccountNameToken, $StorageAccountNameValue -replace $ResourceGroupToken, $ResourceGroupValue;
-$a = $a -replace $AppServiceNameToken, $AppServiceNameValue -replace $ResourceGroupToken, $ResourceGroupValue;
-$a = $a -replace $SqlServerNameToken, $SqlServerNameValue -replace $SqlServerElasticPoolToken, $SqlServerElasticPoolValue;
-$a = $a -replace $SqlServerDatabaseNameToken, $SqlServerDatabaseNameValue -replace $SqlServerAuditStorageUrlToken, $SqlServerAuditStorageUrlValue;
-$a = $a -replace $AppInsightsSpaNameToken, $AppInsightsSpaNameValue;
+$a = $a -replace $ResourceGroupToken, $ResourceGroupValue;
 Set-Content -Value $a -Path $MasterParametersTemplate;
 Write-Host "Complete!";
 
@@ -56,29 +48,5 @@ Write-Host "Setting Static Website url for origin endpoint to CDN";
 $StorageAccountStaticWebsiteUrl = az storage account show --name $ResourceGroup.ToLower() --resource-group $ResourceGroup --query "primaryEndpoints.web" --output tsv;
 $CDNProfileEndpointOriginValue = $StorageAccountStaticWebsiteUrl -replace 'https:', '' -replace '/', '' -replace 'http:', '';
 Write-Host "Set! Static Website Url: $($CDNProfileEndpointOriginValue)";
-
-#endregion
-
-#region CDN template
-Write-Host "Replacing tokens on Master CDN Template...";
-$a = Get-Content $CDNTemplate -raw;
-$a = $a -replace $CDNProfileNameToken, $CDNProfileNameValue -replace $CDNProfileEndpointNameToken, $CDNProfileEndpointNameValue;
-$a = $a -replace $CDNProfileEndpointOriginToken, $CDNProfileEndpointOriginValue -replace $ResourceGroupToken, $ResourceGroupValue;
-Set-Content -Value $a -Path $CDNTemplate;
-Write-Host "Complete!";
-
-Write-Host "Replacing tokens on Master CDN Parameters Template...";
-$a = Get-Content $CDNParametersTemplate -raw;
-$a = $a -replace $CDNProfileNameToken, $CDNProfileNameValue -replace $CDNProfileEndpointNameToken, $CDNProfileEndpointNameValue;
-$a = $a -replace $CDNProfileEndpointOriginToken, $CDNProfileEndpointOriginValue -replace $ResourceGroupToken, $ResourceGroupValue;
-Set-Content -Value $a -Path $CDNParametersTemplate;
-Write-Host "Complete!";
-
-Write-Host "Deploying CDN Template to Resource Group: $($ResourceGroup)";
-$CDNTemplateDeploy = az deployment group create --resource-group $ResourceGroup --template-file $CDNTemplate --parameters $CDNParametersTemplate --name "$($DeploymentName)-CDN" --query "properties.provisioningState";
-Write-Host "CDN Template Deploy: $($CDNTemplateDeploy)";
-$CDNHostName = az cdn endpoint show --name $ResourceGroupValue --profile-name $ResourceGroupValue --resource-group $ResourceGroupValue --query "hostName" --output tsv;
-$CDNHttpsEndpoint = "https://" + $CDNHostName + "/";
-Write-Host "CDN Endpoint: $($CDNHttpsEndpoint)";
 
 #endregion
