9e48373592ebe8e7bada7f0315dc36cb0629a80f
Cal Hueber <Cal.Hueber@sopheon.com>
2021-07-15 09:19:55 -0600

    Updated to look and replace BrowswerWebAppUrl token

--- Scripts/Replace_Environment_Tokens_ReactApp.ps1
+++ Scripts/Replace_Environment_Tokens_ReactApp.ps1
@@ -7,6 +7,7 @@ Write-Host "Running through files: $($childFiles.Length)";
 
 $ShellClientId = (az keyvault secret show --vault-name "Stratus-$($Environment)" --name "StratusB2CShellAppClientId" --query value).Replace('"', '');
 $TenantName = (az keyvault secret show --vault-name "Stratus-$($Environment)" --name "StratusB2CTenantName" --query value).Replace('"', '');
+$BrowswerWebAppUrl = (az keyvault secret show --vault-name "Stratus-$($Environment)" --name "StratusBrowserWebAppUrl" --query value).Replace('"', '');
 $LoginName = $TenantName.Replace(".onmicrosoft.com", "");
 
 
@@ -22,12 +23,19 @@ $childFiles | ForEach-Object -Process {
         Write-Host "Replaced Shell App Client Id in file: $($_.FullName)";
     }
 
-    Write-Host "Looking for Token: 'B2CTenantName'";
+    Write-Host "Looking for Token: '&B2CTenantName&'";
     if($rawData -match "&B2CTenantName&") {        
         $isDataTouched = $true;
         $rawData = $rawData -replace "&B2CTenantName&", $LoginName;
         Write-Host "Replaced Tenant Name in file: $($_.FullName)";
-    }   
+    }
+    
+    Write-Host "Looking for Token: '&BrowserWebAppUrl&'";
+    if($rawData -match "&BrowserWebAppUrl&") {        
+        $isDataTouched = $true;
+        $rawData = $rawData -replace "&BrowserWebAppUrl&", $BrowswerWebAppUrl;
+        Write-Host "Replaced Tenant Name in file: $($_.FullName)";
+    }  
 
     if($isDataTouched) {
         Write-Host "Is data touched, replacing data in file"
