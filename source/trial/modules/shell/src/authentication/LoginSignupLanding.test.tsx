285385c9856f4e9437454c4a80826d0dfd070728
Brian Foster <brian.foster@sopheon.com>
2021-07-19 15:21:11 -0600

    Cloud-1190 update LoginSignupLanding tests

--- source/trial/modules/shell/src/authentication/LoginSignupLanding.test.tsx
+++ source/trial/modules/shell/src/authentication/LoginSignupLanding.test.tsx
@@ -9,7 +9,7 @@ import LoginSignupLanding, { LandingMode } from './LoginSignupLanding';
 expect.extend(toHaveNoViolations);
 
 describe('Test LoginSignupLanding component', () => {
-  it('LoginSignupLanding component renders and is a11y compliant', async () => {
+  it('LoginSignupLanding component in Login LandingMode renders and is a11y compliant', async () => {
     const wrapper = mount(
       <IntlProvider locale="en" messages={messages.en}>
         <LoginSignupLanding landingMode={LandingMode.Login} />
@@ -23,7 +23,24 @@ describe('Test LoginSignupLanding component', () => {
     });
 
     expect(wrapper.find('.ms-Spinner')).toHaveLength(1);
-    expect(wrapper.find('.ms-Spinner-label').text()).toEqual(messages.en['loginsignuplanding.enteringflow']);
+    expect(wrapper.find('.ms-Spinner-label').text()).toEqual(messages.en['loginsignuplanding.loginspinner']);
+    expect(axeResults).toHaveNoViolations();
+  });
+  it('LoginSignupLanding component in Signup LandingMode renders and is a11y compliant', async () => {
+    const wrapper = mount(
+      <IntlProvider locale="en" messages={messages.en}>
+        <LoginSignupLanding landingMode={LandingMode.Signup} />
+      </IntlProvider>
+    );
+    const axeResults = await axe(wrapper.getDOMNode(), {
+      rules: {
+        // LoginSignupLanding page is correctly rendered in <body> region at runtime.
+        region: { enabled: false },
+      },
+    });
+
+    expect(wrapper.find('.ms-Spinner')).toHaveLength(1);
+    expect(wrapper.find('.ms-Spinner-label').text()).toEqual(messages.en['loginsignuplanding.signupspinner']);
     expect(axeResults).toHaveNoViolations();
   });
 });
