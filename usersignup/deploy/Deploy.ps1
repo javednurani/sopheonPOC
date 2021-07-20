72632dbda1dbfbd73c01211c357764a186057ba0
Cal Hueber <Cal.Hueber@sopheon.com>
2021-07-20 15:16:10 -0600

    Returned to working order and updated other PS.exe calls

--- usersignup/deploy/Deploy.ps1
+++ usersignup/deploy/Deploy.ps1
@@ -8,22 +8,22 @@ Write-Host "Zipping Artfacts for UserSignUp...";
 & $ZipUtil "x" "$($PSScriptRoot)/UserSignUpSignIn_*.zip" "-o$($B2CAssets)";
 & $ZipUtil "x" "$($env:System_DefaultWorkingDirectory)/_DevOpsScripts/DevOps/PowerShell_Scripts_*.zip" "-o$($Scripts)";
 
+$ErrorActionPreference = 'Stop'
 Write-Host "...Replacing Configuration Variables on website/index.html...";
 # Replace variables on index.html 
-powershell.exe -file "$($Scripts)/Replace_Config_Variables.ps1" -PathToFile "$($B2CAssets)/website/index.html";
+powershell.exe -file "$($Scripts)/Replace_Config_Variables.ps1" -PathToFile "$($B2CAssets)/website/index.html" 2>&1;
 
 Write-Host "...Replacing Environment Tokens on azureResources/termsOfUse/consentPage.html...";
 # Replace tokens on constentPage.html 
-powershell.exe -file "$($Scripts)/Replace_Environment_Tokens.ps1" -PathToFile "$($B2CAssets)/azureResources/termsOfUse/consentPage.html";
+powershell.exe -file "$($Scripts)/Replace_Environment_Tokens.ps1" -PathToFile "$($B2CAssets)/azureResources/termsOfUse/consentPage.html" 2>&1;
 
 Write-Host "...Replacing Environment Tokens on azureResources/Login/LoginCustom.html...";
-$ErrorActionPreference = 'Stop'
 # Replace tokens on LoginCustom.html 
-$output = powershell.exe -file "$($Scripts)/Replace_Environment_Tokens.ps1" -PathToFile "$($B2CAssets)/Login/LoginCustom.html" 2>&1;
+powershell.exe -file "$($Scripts)/Replace_Environment_Tokens.ps1" -PathToFile "$($B2CAssets)/azureResources/Login/LoginCustom.html" 2>&1;
 
 Write-Host "...Replacing Environment Tokens on azureResources/TrustFrameworkBase.xml...";
 # Replace tokens on TrustFrameworkBase.xml 
-powershell.exe -file "$($Scripts)/Replace_Environment_Tokens.ps1" -PathToFile "$($B2CAssets)/azureResources/TrustFrameworkBase.xml";
+powershell.exe -file "$($Scripts)/Replace_Environment_Tokens.ps1" -PathToFile "$($B2CAssets)/azureResources/TrustFrameworkBase.xml" 2>&1;
 
 
 # Upload the TrustFramework .xml files from the GraphApi...
