0e43a68d1d1e5e57d8d65c5edc773cdec897c0b0
Cal Hueber <Cal.Hueber@sopheon.com>
2021-07-20 14:48:43 -0600

    Test exit on failure

--- usersignup/deploy/Deploy.ps1
+++ usersignup/deploy/Deploy.ps1
@@ -2,6 +2,7 @@ $ZipUtil = "C:\Program Files\7-Zip\7z.exe";
 $B2CAssets = "$($env:System_DefaultWorkingDirectory)/B2CAssets";
 $Scripts = "$($env:System_DefaultWorkingDirectory)/Scripts";
 
+try {
 # Zip/Archive Scripts 
 Write-Host "Zipping Artfacts for UserSignUp...";
 & $ZipUtil "x" "$($PSScriptRoot)/UserSignUpSignIn_*.zip" "-o$($B2CAssets)";
@@ -17,7 +18,7 @@ powershell.exe -file "$($Scripts)/Replace_Environment_Tokens.ps1" -PathToFile "$
 
 Write-Host "...Replacing Environment Tokens on azureResources/Login/LoginCustom.html...";
 # Replace tokens on LoginCustom.html 
-powershell.exe -file "$($Scripts)/Replace_Environment_Tokens.ps1" -PathToFile "$($B2CAssets)/azureResources/Login/LoginCustom.html";
+powershell.exe -file "$($Scripts)/Replace_Environment_Tokens.ps1" -PathToFile "$($B2CAssets)/Login/LoginCustom.html";
 
 Write-Host "...Replacing Environment Tokens on azureResources/TrustFrameworkBase.xml...";
 # Replace tokens on TrustFrameworkBase.xml 
@@ -25,3 +26,8 @@ powershell.exe -file "$($Scripts)/Replace_Environment_Tokens.ps1" -PathToFile "$
 
 
 # Upload the TrustFramework .xml files from the GraphApi...
+
+
+} catch {
+    exit 1;
+}
