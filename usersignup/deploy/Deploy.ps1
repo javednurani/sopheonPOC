5d2dd3a2a4c6ec294197d650daf596b8142c3182
Cal Hueber <Cal.Hueber@sopheon.com>
2021-07-20 14:17:43 -0600

    Testing replace config variables

--- usersignup/deploy/Deploy.ps1
+++ usersignup/deploy/Deploy.ps1
@@ -1,7 +1,11 @@
 $ZipUtil = "C:\Program Files\7-Zip\7z.exe";
-
-
+$B2CAssets = "$($env:System_DefaultWorkingDirectory)/B2CAssets";
+$Scripts = "$($env:System_DefaultWorkingDirectory)/Scripts";
 
 # Zip/Archive Scripts 
 Write-Host "Zipping Artfacts for UserSignUp...";
-& $ZipUtil "x" "$($PSScriptRoot)/UserSignUpSignIn_*.zip" "-o$($env:System_DefaultWorkingDirectory)/B2CAssets";
+& $ZipUtil "x" "UserSignUpSignIn_*.zip" "-o$($B2CAssets)";
+& $ZipUtil "x" "$($env:System_DefaultWorkingDirectory)/_DevOpsScripts/DevOps/PowerShell_Scripts_*.zip" "-o$($Scripts)";
+
+# Replace variables on index.html 
+& "$($Scripts)/Replace_Config_Variables.ps1 -PathToFile $($B2CAssets)/website/index.html";
