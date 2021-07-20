8d3955d46237f94da51c879be01c551c07b8995a
Cal Hueber <Cal.Hueber@sopheon.com>
2021-07-20 14:24:21 -0600

    Test parameters with PS call

--- usersignup/deploy/Deploy.ps1
+++ usersignup/deploy/Deploy.ps1
@@ -4,8 +4,8 @@ $Scripts = "$($env:System_DefaultWorkingDirectory)/Scripts";
 
 # Zip/Archive Scripts 
 Write-Host "Zipping Artfacts for UserSignUp...";
-& $ZipUtil "x" "UserSignUpSignIn_*.zip" "-o$($B2CAssets)";
+& $ZipUtil "x" "$($PSScriptRoot)/UserSignUpSignIn_*.zip" "-o$($B2CAssets)";
 & $ZipUtil "x" "$($env:System_DefaultWorkingDirectory)/_DevOpsScripts/DevOps/PowerShell_Scripts_*.zip" "-o$($Scripts)";
 
 # Replace variables on index.html 
-& "$($Scripts)/Replace_Config_Variables.ps1 -PathToFile $($B2CAssets)/website/index.html";
+& "$($Scripts)/Replace_Config_Variables.ps1" -PathToFile $($B2CAssets)/website/index.html;
