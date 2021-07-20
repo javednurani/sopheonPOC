f995ae9e811a4da0ede4a8880cbcda27690ccaa7
Cal Hueber <Cal.Hueber@sopheon.com>
2021-07-20 12:33:34 -0600

    replace . with _

--- usersignup/build/Build.ps1
+++ usersignup/build/Build.ps1
@@ -2,6 +2,6 @@ $ZipUtil = "C:\Program Files\7-Zip\7z.exe";
 
 # Zip/Archive Scripts 
 Write-Host "Zipping Artfacts for UserSignUp...";
-& $ZipUtil "a" "-tzip" "$($env:Build.ArtifactStagingDirectory)/UserSignUpSignIn_$($env:Build.BuildId)" "$($env:System.DefaultWorkingDirectory)/usersignup";
+& $ZipUtil "a" "-tzip" "$($env:Build_ArtifactStagingDirectory)/UserSignUpSignIn_$($env:Build_BuildId)" "$($env:System_DefaultWorkingDirectory)/usersignup";
 
 Write-Host "Zipping Complete!";
