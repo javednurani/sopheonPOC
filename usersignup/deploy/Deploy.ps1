666ed477464c71069617dce0f832547efeaeabd3
Cal Hueber <Cal.Hueber@sopheon.com>
2021-07-20 14:44:10 -0600

    added other calls for replace

--- usersignup/deploy/Deploy.ps1
+++ usersignup/deploy/Deploy.ps1
@@ -7,5 +7,21 @@ Write-Host "Zipping Artfacts for UserSignUp...";
 & $ZipUtil "x" "$($PSScriptRoot)/UserSignUpSignIn_*.zip" "-o$($B2CAssets)";
 & $ZipUtil "x" "$($env:System_DefaultWorkingDirectory)/_DevOpsScripts/DevOps/PowerShell_Scripts_*.zip" "-o$($Scripts)";
 
+Write-Host "...Replacing Configuration Variables on website/index.html...";
 # Replace variables on index.html 
 powershell.exe -file "$($Scripts)/Replace_Config_Variables.ps1" -PathToFile "$($B2CAssets)/website/index.html";
+
+Write-Host "...Replacing Environment Tokens on termsOfUse/consentPage.html...";
+# Replace tokens on constentPage.html 
+powershell.exe -file "$($Scripts)/Replace_Environment_Tokens.ps1" -PathToFile "$($B2CAssets)/termsOfUse/consentPage.html";
+
+Write-Host "...Replacing Environment Tokens on Login/LoginCustom.html...";
+# Replace tokens on LoginCustom.html 
+powershell.exe -file "$($Scripts)/Replace_Environment_Tokens.ps1" -PathToFile "$($B2CAssets)/Login/LoginCustom.html";
+
+Write-Host "...Replacing Environment Tokens on TrustFrameworkBase.xml...";
+# Replace tokens on TrustFrameworkBase.xml 
+powershell.exe -file "$($Scripts)/Replace_Environment_Tokens.ps1" -PathToFile "$($B2CAssets)/TrustFrameworkBase.xml";
+
+
+# Upload the TrustFramework .xml files from the GraphApi...
