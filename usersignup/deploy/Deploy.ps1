29ab72b44f5fb431e8993e97f9c762e974e3e15a
Cal Hueber <Cal.Hueber@sopheon.com>
2021-07-20 12:58:36 -0600

    Test deploy script

--- usersignup/deploy/Deploy.ps1
+++ usersignup/deploy/Deploy.ps1
@@ -1,1 +1,7 @@
 $ZipUtil = "C:\Program Files\7-Zip\7z.exe";
+
+
+
+# Zip/Archive Scripts 
+Write-Host "Zipping Artfacts for UserSignUp...";
+& $ZipUtil "x" "$($env:System_DefaultWorkingDirectory)/_StratusB2CAssets/UserSignUpSignIn_*.zip" "-o$($env:System_DefaultWorkingDirectory)/B2CAssets";
