447d7fadb21062c248264a8a88cc141759f5fb49
Cal Hueber <Cal.Hueber@sopheon.com>
2021-07-20 13:00:59 -0600

    changed to ps script root

--- usersignup/deploy/Deploy.ps1
+++ usersignup/deploy/Deploy.ps1
@@ -4,4 +4,4 @@ $ZipUtil = "C:\Program Files\7-Zip\7z.exe";
 
 # Zip/Archive Scripts 
 Write-Host "Zipping Artfacts for UserSignUp...";
-& $ZipUtil "x" "$($env:System_DefaultWorkingDirectory)/_StratusB2CAssets/UserSignUpSignIn_*.zip" "-o$($env:System_DefaultWorkingDirectory)/B2CAssets";
+& $ZipUtil "x" "$($PSScriptRoot)/UserSignUpSignIn_*.zip" "-o$($env:System_DefaultWorkingDirectory)/B2CAssets";
