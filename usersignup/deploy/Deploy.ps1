0f5c11fb0cc3d2191cc11d7fd1ba642f0b79d23e
Cal Hueber <Cal.Hueber@sopheon.com>
2021-07-26 07:01:42 -0600

    removed stdout to file 1

--- usersignup/deploy/Deploy.ps1
+++ usersignup/deploy/Deploy.ps1
@@ -13,34 +13,34 @@ Write-Host "Zipping Artfacts for UserSignUp...";
 
 Write-Host "...Replacing Configuration Variables on website/index.html...";
 # Replace variables on index.html 
-powershell.exe -file "$($Scripts)/Replace_Config_Variables.ps1" -PathToFile "$($B2CAssets)/website/index.html" 2>&1;
+powershell.exe -file "$($Scripts)/Replace_Config_Variables.ps1" -PathToFile "$($B2CAssets)/website/index.html";
 
 Write-Host "...Replacing Environment Tokens on azureResources/termsOfUse/consentPage.html...";
 # Replace tokens on constentPage.html 
-powershell.exe -file "$($Scripts)/Replace_Environment_Tokens.ps1" -PathToFile "$($B2CAssets)/azureResources/termsOfUse/consentPage.html" 2>&1;
+powershell.exe -file "$($Scripts)/Replace_Environment_Tokens.ps1" -PathToFile "$($B2CAssets)/azureResources/termsOfUse/consentPage.html";
 
 Write-Host "...Replacing Environment Tokens on azureResources/Login/LoginCustom.html...";
 # Replace tokens on LoginCustom.html 
-powershell.exe -file "$($Scripts)/Replace_Environment_Tokens.ps1" -PathToFile "$($B2CAssets)/azureResources/Login/LoginCustom.html" 2>&1;
+powershell.exe -file "$($Scripts)/Replace_Environment_Tokens.ps1" -PathToFile "$($B2CAssets)/azureResources/Login/LoginCustom.html";
 
 Write-Host "...Replacing Environment Tokens on azureResources/TrustFrameworkBase.xml...";
 # Replace tokens on TrustFrameworkBase.xml 
-powershell.exe -file "$($Scripts)/Replace_Environment_Tokens.ps1" -PathToFile "$($B2CAssets)/azureResources/TrustFrameworkBase.xml" 2>&1;
+powershell.exe -file "$($Scripts)/Replace_Environment_Tokens.ps1" -PathToFile "$($B2CAssets)/azureResources/TrustFrameworkBase.xml";
 
 
 # Upload the TrustFramework .xml files from the GraphApi...
 
 Write-Host "...Uploading Policy: B2C_1A_TrustFrameworkBase via GraphAPI...";
-powershell.exe -file "$($Scripts)/Deploy_B2C_Assets.ps1" -PolicyId B2C_1A_TrustFrameworkBase -PathToFile "$($B2CAssets)/azureResources/TrustFrameworkBase.xml" -Environment $Environment 2>&1;
+powershell.exe -file "$($Scripts)/Deploy_B2C_Assets.ps1" -PolicyId B2C_1A_TrustFrameworkBase -PathToFile "$($B2CAssets)/azureResources/TrustFrameworkBase.xml" -Environment $Environment;
 
 Write-Host "...Uploading Policy: B2C_1A_TrustFrameworkExtensions via GraphAPI...";
-powershell.exe -file "$($Scripts)/Deploy_B2C_Assets.ps1" -PolicyId B2C_1A_TrustFrameworkExtensions -PathToFile "$($B2CAssets)/azureResources/TrustFrameworkExtensions.xml" -Environment $Environment 2>&1;
+powershell.exe -file "$($Scripts)/Deploy_B2C_Assets.ps1" -PolicyId B2C_1A_TrustFrameworkExtensions -PathToFile "$($B2CAssets)/azureResources/TrustFrameworkExtensions.xml" -Environment $Environment;
 
 Write-Host "...Uploading Policy: B2C_1A_signup via GraphAPI...";
-powershell.exe -file "$($Scripts)/Deploy_B2C_Assets.ps1" -PolicyId B2C_1A_signup -PathToFile "$($B2CAssets)/azureResources/SignUp.xml" -Environment $Environment 2>&1;
+powershell.exe -file "$($Scripts)/Deploy_B2C_Assets.ps1" -PolicyId B2C_1A_signup -PathToFile "$($B2CAssets)/azureResources/SignUp.xml" -Environment $Environment;
 
 Write-Host "...Uploading Policy: B2C_1A_signup_signin via GraphAPI...";
-powershell.exe -file "$($Scripts)/Deploy_B2C_Assets.ps1" -PolicyId B2C_1A_signup_signin -PathToFile "$($B2CAssets)/azureResources/SignUpOrSignin.xml" -Environment $Environment 2>&1;
+powershell.exe -file "$($Scripts)/Deploy_B2C_Assets.ps1" -PolicyId B2C_1A_signup_signin -PathToFile "$($B2CAssets)/azureResources/SignUpOrSignin.xml" -Environment $Environment;
 
 Write-Host "Uploading Marketing Page to blob storage";
 $MarketingUploadResults = az storage blob upload --container-name '$web' --account-name $StorageAccountName --file "$($B2CAssets)/website/index.html" --name index.html --auth-mode login;
