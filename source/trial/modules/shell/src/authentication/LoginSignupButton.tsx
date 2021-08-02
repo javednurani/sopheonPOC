42fd2ff80951ca0bfd7ac1f024efb22f831860d1
Brian Foster <brian.foster@sopheon.com>
2021-08-02 16:40:17 -0600

    Cloud-1326 add change password to spa

--- source/trial/modules/shell/src/authentication/LoginSignupButton.tsx
+++ source/trial/modules/shell/src/authentication/LoginSignupButton.tsx
@@ -3,15 +3,45 @@ import { DefaultButton, IContextualMenuProps } from '@fluentui/react';
 import React, { FunctionComponent } from 'react';
 import { useIntl } from 'react-intl';
 
+import azureSettings from '../azureSettings';
+
 const LoginSignupButton: FunctionComponent = () => {
   const { formatMessage } = useIntl();
   const { instance, accounts } = useMsal();
+
+  const changePasswordClick = () => {
+    // TODO, this will be refactored once main branch (with Cloud-1214 work) is merged into Cloud-1275 story branch
+    // ternary statements will be replaced by logic in azureSettings, and getAuthorityUrl helper function will be used
+    const isDev = process.env.NODE_ENV === 'development';
+    const redirectUri: string = isDev ? azureSettings.SPA_Root_URL_Dev : azureSettings.SPA_Root_URL;
+
+    const adB2cTenantName: string = isDev ? azureSettings.AD_B2C_TenantName_Dev : azureSettings.AD_B2C_TenantName;
+    const passwordChangeAuthorityUrl = `https://${adB2cTenantName}.b2clogin.com/${adB2cTenantName}.onmicrosoft.com/${azureSettings.AD_B2C_PasswordChange_Policy}`;
+
+    // Cloud-1339: The below ts-ignore is due to not including a 'scopes' property in the RedirectRequest object
+    // The linked example code from Microsoft, demo'ing a loginRedirect Profile Edit, does not include 'scopes' on the RedirectRequest
+    // This call to Password Change is similar, in that a loginRedirect call is used to access a non-signupsignin flow
+    // https://github.com/Azure-Samples/ms-identity-b2c-javascript-spa/blob/main/App/authRedirect.js
+    // @ts-ignore
+    instance.loginRedirect({
+      authority: passwordChangeAuthorityUrl,
+      redirectUri: redirectUri,
+    });
+  };
+
   const menuProps: IContextualMenuProps = {
     items: [
       {
         key: 'profile',
         text: formatMessage({ id: 'auth.myprofile' }),
         iconProps: { iconName: 'EditContact' },
+      },
+      {
+        key: 'changepassword',
+        // TODO-1214 LOCALIZE
+        text: formatMessage({ id: 'auth.changepassword' }),
+        iconProps: { iconName: 'EditContact' },
+        onClick: changePasswordClick,
       },
       {
         key: 'signout',
--- source/trial/modules/shell/src/azureSettings.ts
+++ source/trial/modules/shell/src/azureSettings.ts
@@ -5,6 +5,7 @@ const azureSettings: Record<string, string> = {
   AD_B2C_TenantName_Dev: 'StratusB2CDev',
   AD_B2C_SignUpSignIn_Policy: 'B2C_1A_SIGNUP_SIGNIN',
   AD_B2C_SignUp_Policy: 'B2C_1A_SIGNUP',
+  AD_B2C_PasswordChange_Policy: 'B2C_1A_PROFILEEDIT_PASSWORDCHANGE',
   SPA_Root_URL: 'https://^BrowserWebAppUrl^.azureedge.net/',
   SPA_Root_URL_Dev: 'https://localhost:3000/',
 };
