c0d1c2aafe0decc26921c7292a1577b4fd2fe4f9
Brian Foster <brian.foster@sopheon.com>
2021-08-03 14:34:07 -0600

    Cloud-1345 refactor 1275 and 1214 msal work

--- source/trial/modules/shell/src/authentication/LoginSignupButton.tsx
+++ source/trial/modules/shell/src/authentication/LoginSignupButton.tsx
@@ -4,7 +4,7 @@ import { DefaultButton, IContextualMenuProps } from '@fluentui/react';
 import React, { FunctionComponent, useEffect, useState } from 'react';
 import { useIntl } from 'react-intl';
 
-import { azureSettings, getAuthorityUrl } from '../azureSettings';
+import { changePasswordRequest, editProfileRequest, setMsalAccount } from './authHelpers';
 
 const LoginSignupButton: FunctionComponent = () => {
   const { formatMessage } = useIntl();
@@ -13,75 +13,15 @@ const LoginSignupButton: FunctionComponent = () => {
   const [account, setAccount] = useState<AccountInfo>();
 
   useEffect(() => {
-    setMsalAccount();
-  }, [accounts]);
-
-  // Important to set MSAL account correctly - "accounts" provided by useMsal() hook can include auth responses from initiating any user flow,
-  // including profile edit, password change, etc.  We need the account / auth response returned from the SignUpSignIn flow.
-  // See MS example code with a version of this function
-  // https://github.com/Azure-Samples/ms-identity-b2c-javascript-spa/blob/main/App/authRedirect.js
-  const setMsalAccount = () => {
-    /**
-     * See here for more information on account retrieval:
-     * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-common/docs/Accounts.md
-     */
-
-    const currentMsalAccounts = instance.getAllAccounts();
-
-    if (currentMsalAccounts.length < 1) {
-      return;
-    } else if (currentMsalAccounts.length > 1) {
-      /**
-       * Due to the way MSAL caches account objects, the auth response from initiating a user-flow
-       * is cached as a new account, which results in more than one account in the cache. Here we make
-       * sure we are selecting the account with homeAccountId that contains the sign-up/sign-in user-flow,
-       * as this is the default flow the user initially signed-in with.
-       */
-      const adB2cAuthorityDomain = `${azureSettings.AD_B2C_TenantName}.b2clogin.com`;
-
-      const msalAccounts = currentMsalAccounts.filter(
-        msalAccount =>
-          msalAccount.homeAccountId.toUpperCase().includes(azureSettings.AD_B2C_SignUpSignIn_Policy.toUpperCase()) &&
-          msalAccount.idTokenClaims !== undefined &&
-          // AccountInfo.idTokenClaims is typed as 'object' by Microsoft
-          // @ts-ignore
-          msalAccount.idTokenClaims.iss.toUpperCase().includes(adB2cAuthorityDomain.toUpperCase()) &&
-          // AccountInfo.idTokenClaims is typed as 'object' by Microsoft
-          // @ts-ignore
-          msalAccount.idTokenClaims.aud === azureSettings.AD_B2C_ClientId
-      );
-
-      if (msalAccounts.length > 1) {
-        // localAccountId identifies the entity for which the token asserts information.
-        if (msalAccounts.every(msalAccount => msalAccount.localAccountId === msalAccounts[0].localAccountId)) {
-          // All accounts belong to the same user
-          setAccount(msalAccounts[0]);
-        } else {
-          // Multiple users detected. Logout all to be safe.
-          instance.logout();
-        }
-      } else if (msalAccounts.length === 1) {
-        setAccount(msalAccounts[0]);
-      }
-    } else if (currentMsalAccounts.length === 1) {
-      setAccount(currentMsalAccounts[0]);
-    }
-  };
+    setMsalAccount(instance, setAccount);
+  }, [instance, accounts]);
 
   const changePasswordClick = () => {
-    instance.loginRedirect({
-      authority: getAuthorityUrl(azureSettings.AD_B2C_PasswordChange_Policy),
-      redirectUri: azureSettings.SPA_Root_URL,
-      scopes: [],
-    });
+    instance.loginRedirect(changePasswordRequest);
   };
 
   const editProfileClick = () => {
-    instance.loginRedirect({
-      authority: getAuthorityUrl(azureSettings.AD_B2C_ProfileEdit_Policy),
-      redirectUri: azureSettings.SPA_Root_URL,
-      scopes: [],
-    });
+    instance.loginRedirect(editProfileRequest);
   };
 
   const menuProps: IContextualMenuProps = {
---
+++ source/trial/modules/shell/src/authentication/authHelpers.ts
@@ -0,0 +1,75 @@
+import { AccountInfo, IPublicClientApplication, RedirectRequest } from '@azure/msal-browser';
+
+import { azureSettings, getAuthorityUrl } from '../azureSettings';
+
+
+export const changePasswordRequest: RedirectRequest = {
+  authority: getAuthorityUrl(azureSettings.AD_B2C_PasswordChange_Policy),
+  redirectUri: azureSettings.SPA_Root_URL,
+  scopes: [],
+};
+
+export const editProfileRequest: RedirectRequest = {
+  authority: getAuthorityUrl(azureSettings.AD_B2C_ProfileEdit_Policy),
+  redirectUri: azureSettings.SPA_Root_URL,
+  scopes: [],
+};
+
+export const getAuthLandingRedirectRequest = (adB2cPolicyName: string): RedirectRequest => ({
+  authority: getAuthorityUrl(adB2cPolicyName),
+  scopes: ['openid', 'offline_access'],
+  redirectUri: azureSettings.SPA_Root_URL,
+  redirectStartPage: azureSettings.SPA_Root_URL,
+});
+
+// Important to set MSAL account correctly - "accounts" provided by useMsal() hook can include auth responses from initiating any user flow,
+// including profile edit, password change, etc.  We need the account / auth response returned from the SignUpSignIn flow.
+// See MS example code with a version of this function
+// https://github.com/Azure-Samples/ms-identity-b2c-javascript-spa/blob/main/App/authRedirect.js
+export const setMsalAccount = (instance: IPublicClientApplication, setAccount: (value: React.SetStateAction<AccountInfo | undefined>) => void): void => {
+  /**
+   * See here for more information on account retrieval:
+   * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-common/docs/Accounts.md
+   */
+
+  const currentMsalAccounts = instance.getAllAccounts();
+
+  if (currentMsalAccounts.length < 1) {
+    return;
+  } else if (currentMsalAccounts.length > 1) {
+    /**
+     * Due to the way MSAL caches account objects, the auth response from initiating a user-flow
+     * is cached as a new account, which results in more than one account in the cache. Here we make
+     * sure we are selecting the account with homeAccountId that contains the sign-up/sign-in user-flow,
+     * as this is the default flow the user initially signed-in with.
+     */
+    const adB2cAuthorityDomain = `${azureSettings.AD_B2C_TenantName}.b2clogin.com`;
+
+    const msalAccounts = currentMsalAccounts.filter(
+      msalAccount =>
+        msalAccount.homeAccountId.toUpperCase().includes(azureSettings.AD_B2C_SignUpSignIn_Policy.toUpperCase()) &&
+        msalAccount.idTokenClaims !== undefined &&
+        // AccountInfo.idTokenClaims is typed as 'object' by Microsoft
+        // @ts-ignore
+        msalAccount.idTokenClaims.iss.toUpperCase().includes(adB2cAuthorityDomain.toUpperCase()) &&
+        // AccountInfo.idTokenClaims is typed as 'object' by Microsoft
+        // @ts-ignore
+        msalAccount.idTokenClaims.aud === azureSettings.AD_B2C_ClientId
+    );
+
+    if (msalAccounts.length > 1) {
+      // localAccountId identifies the entity for which the token asserts information.
+      if (msalAccounts.every(msalAccount => msalAccount.localAccountId === msalAccounts[0].localAccountId)) {
+        // All accounts belong to the same user
+        setAccount(msalAccounts[0]);
+      } else {
+        // Multiple users detected. Logout all to be safe.
+        instance.logout();
+      }
+    } else if (msalAccounts.length === 1) {
+      setAccount(msalAccounts[0]);
+    }
+  } else if (currentMsalAccounts.length === 1) {
+    setAccount(currentMsalAccounts[0]);
+  }
+};
