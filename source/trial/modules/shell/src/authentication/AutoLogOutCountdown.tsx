5ba201eb6176993f4f6a253c665d18e55f689434
Zach Quinn <ZachQuinn@sopheon.com>
2021-08-11 15:52:22 -0600

    cloud-1446 add dialog text

--- source/trial/modules/shell/src/authentication/AutoLogOutCountdown.tsx
+++ source/trial/modules/shell/src/authentication/AutoLogOutCountdown.tsx
@@ -1,15 +1,25 @@
-import { DefaultButton, Dialog, DialogFooter, PrimaryButton } from '@fluentui/react';
+import { DefaultButton, Dialog, DialogFooter, DialogType, PrimaryButton } from '@fluentui/react';
 import React, { FunctionComponent } from 'react';
 
-const AutoLogOutCountdown: FunctionComponent = () =>
-// example: https://developer.microsoft.com/en-us/fluentui#/controls/web/dialog
-  (
-    <Dialog>
+const AutoLogOutCountdown: FunctionComponent = () => {
+  // example: https://developer.microsoft.com/en-us/fluentui#/controls/web/dialog
+
+  const dialogContentProps = {
+    type: DialogType.normal,
+    title: 'Title??',   // TODO: cloud-1446
+    subText: 'Are you still working? You will be logged out in XX seconds.',    // TODO: cloud-1446
+  };
+
+  return (
+    <Dialog
+      dialogContentProps={dialogContentProps}
+    >
       <DialogFooter>
         <PrimaryButton text="Yes" />
         <DefaultButton text="No" />
       </DialogFooter>
     </Dialog>
   );
+};
 
 export default AutoLogOutCountdown;
--- source/trial/modules/shell/src/settings/appSettings.ts
+++ source/trial/modules/shell/src/settings/appSettings.ts
@@ -1,3 +1,3 @@
 // IdleMonitor / App timeout settings
-export const autoLogOutTime = 60 * 60 * 1000; // 1 hour timeout
+export const autoLogOutTime = 60 * 1000; // 1 hour timeout
 export const showAutoLogOutWarningThreshhold = 10 * 1000; // show auto log out warning at 10 seconds left
