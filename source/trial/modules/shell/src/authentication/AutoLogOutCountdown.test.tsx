c125e9635eee05c3d6f152265e39e915d53d577c
Zach Quinn <ZachQuinn@sopheon.com>
2021-08-12 09:31:41 -0600

    cloud-1446 add test for yes and no buttons

--- source/trial/modules/shell/src/authentication/AutoLogOutCountdown.test.tsx
+++ source/trial/modules/shell/src/authentication/AutoLogOutCountdown.test.tsx
@@ -1,7 +1,8 @@
+import { messages } from '@sopheon/shared-ui';
 import { axe, toHaveNoViolations } from 'jest-axe';
 import React from 'react';
 
-import { render } from '../testUtils';
+import { getInitState, languageRender } from '../testUtils';
 import AutoLogOutCountdown from './AutoLogOutCountDown';
 
 expect.extend(toHaveNoViolations);
@@ -9,10 +10,22 @@ expect.extend(toHaveNoViolations);
 describe('AutoLogOutCountdown', () => {
   test('Has no a11y vialotions.', async () => {
     // Act
-    const { container } = render(<AutoLogOutCountdown />);
+    const { container } = languageRender(<AutoLogOutCountdown />, getInitState({}));
     const axeResults = await axe(container);
 
     // Assert
     expect(axeResults).toHaveNoViolations();
+  });
+
+  test('To have Yes and No buttons', async () => {
+    // Act
+    const { getByText } = languageRender(<AutoLogOutCountdown />, getInitState({}));
+
+    // Assert
+    const yesButton: HTMLElement = getByText(messages.en.yes);
+    const noButton: HTMLElement = getByText(messages.en.no);
+
+    expect(yesButton).toBeInTheDocument();
+    expect(noButton).toBeInTheDocument();
   });
 });
