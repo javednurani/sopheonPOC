2fed1aef2dcc235a3ccc6e8ffe2c2419363dc281
Ryan Weissman <ryan.weissman@sopheon.com>
2021-08-12 15:01:58 -0600

    cloud-1450 IdleMonitor shows dialog onIdle

--- source/trial/modules/shell/src/authentication/IdleMonitor.tsx
+++ source/trial/modules/shell/src/authentication/IdleMonitor.tsx
@@ -1,5 +1,6 @@
 import { IPublicClientApplication } from '@azure/msal-browser';
 import { AuthenticatedTemplate, useMsal } from '@azure/msal-react';
+import { useBoolean } from '@fluentui/react-hooks';
 import React, { FunctionComponent } from 'react';
 import { useIdleTimer } from 'react-idle-timer';
 
@@ -7,18 +8,18 @@ import { IdleTimeoutSettings } from './../settings/appSettings';
 import AutoLogOutCountdown from './AutoLogOutCountdown';
 
 // log out any active accounts then the idle timeout limit is reached
-export const handleOnIdle = (instance: IPublicClientApplication): void => {
-  if (instance.getAllAccounts().length > 0) {
-    instance.logoutRedirect();
+export const handleOnIdle = (msalInstance: IPublicClientApplication, toggleHideDialog: () => void): void => {
+  if (msalInstance.getAllAccounts().length > 0) {
+    toggleHideDialog();
   }
 };
-
 const IdleMonitor: FunctionComponent = () => {
   const { instance } = useMsal();
+  const [hideDialog, { toggle: toggleHideDialog }] = useBoolean(true);
 
   useIdleTimer({
     timeout: (IdleTimeoutSettings.IdleLogOutSeconds - IdleTimeoutSettings.IdleLogOutWarningSeconds) * 1000, // time til show warning converted from s to ms
-    onIdle: () => handleOnIdle(instance),
+    onIdle: () => handleOnIdle(instance, toggleHideDialog),
     crossTab: {
       emitOnAllTabs: true, // prevents user from being logged out of a second app tab
     },
@@ -26,9 +27,7 @@ const IdleMonitor: FunctionComponent = () => {
 
   return (
     <AuthenticatedTemplate>
-      <div data-testid="idleCountdown">
-        <AutoLogOutCountdown />
-      </div>
+      <AutoLogOutCountdown hidden={hideDialog} />
     </AuthenticatedTemplate>
   );
 };
