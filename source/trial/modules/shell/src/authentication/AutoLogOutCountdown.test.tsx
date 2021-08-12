066a64ebe2869ba2986b7dd9825a022d0aaa27e9
Ryan Weissman <ryan.weissman@sopheon.com>
2021-08-12 12:01:32 -0600

    cloud-1449 add test for No button click

--- source/trial/modules/shell/src/authentication/AutoLogOutCountdown.test.tsx
+++ source/trial/modules/shell/src/authentication/AutoLogOutCountdown.test.tsx
@@ -13,6 +13,16 @@ import AutoLogOutCountdown from './AutoLogOutCountdown';
 expect.extend(toHaveNoViolations);
 
 describe('AutoLogOutCountdown', () => {
+  let pca: IPublicClientApplication;
+
+  // Reset the tests
+  beforeEach(() => {
+    pca = testMsalInstance();
+  });
+  afterEach(() => {
+    // cleanup on exiting
+    jest.clearAllMocks();
+  });
   test('Has no a11y vialotions.', async () => {
     // Act
     const { container } = languageRender(<AutoLogOutCountdown />, getInitState({}));
@@ -32,9 +42,29 @@ describe('AutoLogOutCountdown', () => {
     expect(yesButton).toBeInTheDocument();
     expect(noButton).toBeInTheDocument();
   });
+  test('Logout called when No button clicked', async () => {
+    // Arrange
+    const logoutRedirectSpy = jest.spyOn(pca, 'logoutRedirect').mockImplementation(request => {
+      expect(request).toBe(undefined);
+
+      return Promise.resolve();
+    });
+
+    // Act
+    languageRender(
+      <MsalProvider instance={pca}>
+        <AutoLogOutCountdown />
+      </MsalProvider>,
+      getInitState({})
+    );
+    const noButton: HTMLElement = await screen.findByText(messages.en.no);
+    noButton.click();
+
+    // Assert
+    expect(logoutRedirectSpy).toBeCalledTimes(1);
+  });
   test('Countdown timer starts at warning threshold', async () => {
     // Arrange
-    const pca: IPublicClientApplication = testMsalInstance();
     const logoutRedirectSpy = jest.spyOn(pca, 'logoutRedirect').mockImplementation(request => {
       expect(request).toBe(undefined);
 
@@ -54,7 +84,6 @@ describe('AutoLogOutCountdown', () => {
   test('Countdown timer advances properly', async () => {
     // Arrange
     jest.useFakeTimers();
-    const pca: IPublicClientApplication = testMsalInstance();
     const logoutRedirectSpy = jest.spyOn(pca, 'logoutRedirect').mockImplementation(request => {
       expect(request).toBe(undefined);
 
@@ -73,10 +102,11 @@ describe('AutoLogOutCountdown', () => {
     expect(warningText.textContent).not.toContain(showAutoLogOutWarningThreshholdSeconds);
     expect(warningText.textContent).toContain(showAutoLogOutWarningThreshholdSeconds - secondsToAdvance);
     await waitFor(() => expect(logoutRedirectSpy).toBeCalledTimes(0));
+    jest.runOnlyPendingTimers();
+    jest.useRealTimers();
   });
   test('Logout called when timer is 0', async () => {
     // Arrange
-    const pca: IPublicClientApplication = testMsalInstance();
     const logoutRedirectSpy = jest.spyOn(pca, 'logoutRedirect').mockImplementation(request => {
       expect(request).toBe(undefined);
 
