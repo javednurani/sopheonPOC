8771b9058f05b20bf5b66d9afb98cf624cbe5dd5
Ryan Weissman <ryan.weissman@sopheon.com>
2021-08-12 15:00:33 -0600

    cloud-1450 default dialog to hidden and add prop

--- source/trial/modules/shell/src/authentication/AutoLogOutCountdown.tsx
+++ source/trial/modules/shell/src/authentication/AutoLogOutCountdown.tsx
@@ -4,27 +4,29 @@ import React, { FunctionComponent, useEffect } from 'react';
 import { useIntl } from 'react-intl';
 
 import { IdleTimeoutSettings } from './../settings/appSettings';
+export interface AutoLogoutCountdownProps {
+  hidden?: boolean | undefined;
+}
 
-const AutoLogOutCountdown: FunctionComponent = () => {
+const AutoLogOutCountdown: FunctionComponent<AutoLogoutCountdownProps> = ({ hidden }: AutoLogoutCountdownProps) => {
   const { formatMessage } = useIntl();
-  // example: https://developer.microsoft.com/en-us/fluentui#/controls/web/dialog
   const [seconds, setSeconds] = React.useState(IdleTimeoutSettings.IdleLogOutWarningSeconds);
   const { instance } = useMsal();
 
   useEffect(() => {
-    if (seconds > 0) {
+    if (seconds > 0 && !hidden) {
       setTimeout(() => setSeconds(seconds - 1), 1000);
-    } else {
+    } else if (seconds <= 0) {
       instance.logout();
     }
   });
   const dialogContentProps = {
     type: DialogType.normal,
-    subText: `Are you still working? You will be logged out in ${seconds} seconds.`,
+    subText: `Are you still working? You will be logged out in ${seconds} seconds.`, // TODO: Resource this
   };
 
   return (
-    <Dialog hidden={false} dialogContentProps={dialogContentProps}>
+    <Dialog hidden={hidden} dialogContentProps={dialogContentProps}>
       <DialogFooter>
         <PrimaryButton text={formatMessage({ id: 'yes' })} />
         <DefaultButton text={formatMessage({ id: 'no' })} />
