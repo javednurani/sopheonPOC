c3c32f6ed20d93f3cda19e69f53c7cc63601db38
Ryan Weissman <ryan.weissman@sopheon.com>
2021-08-24 09:13:54 -0600

    cloud-1549 Create basic Environment model

---
+++ source/Sopheon.CloudNative.EnvironmentAdmin/Sopheon.CloudNative.EnvironmentAdmin.Data/Models/Environment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Sopheon.CloudNative.EnvironmentAdmin.Data.Models
+{
+   public class Environment
+   {
+      public int EnvironmentID 
+      { 
+         get; 
+         set; 
+      }
+   }
+}
