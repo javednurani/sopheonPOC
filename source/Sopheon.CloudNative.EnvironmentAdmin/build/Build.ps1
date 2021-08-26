2d2c75f5d2a7d8dd07ee66de7fc2f1e97b5686ec
Cal Hueber <Cal.Hueber@sopheon.com>
2021-08-26 13:06:53 -0600

    Added build script for environment management script generation

---
+++ source/Sopheon.CloudNative.EnvironmentAdmin/build/Build.ps1
@@ -0,0 +1,13 @@
+$ZipUtil = "C:\Program Files\7-Zip\7z.exe";
+
+Copy-Item -Path "$($env:System_DefaultWorkingDirectory)/source/Sopheon.CloudNative.EnvironmentAdmin/deploy/*" -Destination "$($env:Build_ArtifactStagingDirectory)";
+
+Set-Location -Path "$($env:System_DefaultWorkingDirectory)/source/Sopheon.CloudNative.EnvironmentAdmin";
+
+dotnet ef migrations script -p "Sopheon.CloudNative.EnvironmentAdmin.Data\Sopheon.CloudNative.EnvironmentAdmin.Data.csproj" -o $(build.artifactstagingdirectory)\scripts.sql -i
+
+# Zip/Archive Scripts 
+Write-Host "Zipping Artfacts for Environment Management...";
+& $ZipUtil "a" "-tzip" "$($env:Build_ArtifactStagingDirectory)/EnvironmentManagement_$($env:Build_BuildId)" "$($env:System_DefaultWorkingDirectory)/source/Sopheon.CloudNative.EnvironmentAdmin/PublishOutput/*" "-xr!build" "-xr!deploy";
+
+Write-Host "Zipping Complete!";
