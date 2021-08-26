0de4580b2436391ce079de7f861b2550fe66ac74
Cal Hueber <Cal.Hueber@sopheon.com>
2021-08-26 13:55:49 -0600

    updated build script with correct paths

--- source/Sopheon.CloudNative.Environments/build/Build.ps1
+++ source/Sopheon.CloudNative.Environments/build/Build.ps1
@@ -1,12 +1,12 @@
 $ZipUtil = "C:\Program Files\7-Zip\7z.exe";
 
-Copy-Item -Path "$($env:System_DefaultWorkingDirectory)/source/Sopheon.CloudNative.EnvironmentAdmin/deploy/*" -Destination $env:Build_ArtifactStagingDirectory;
+Copy-Item -Path "$($env:System_DefaultWorkingDirectory)/source/Sopheon.CloudNative.Environments/deploy/*" -Destination $env:Build_ArtifactStagingDirectory;
 
-Set-Location -Path "$($env:System_DefaultWorkingDirectory)/source/Sopheon.CloudNative.EnvironmentAdmin";
+Set-Location -Path "$($env:System_DefaultWorkingDirectory)/source/Sopheon.CloudNative.Environments";
 
 #mkdir PublishOutput
 
-dotnet ef migrations script -p "Sopheon.CloudNative.EnvironmentAdmin.Data\Sopheon.CloudNative.EnvironmentAdmin.Data.csproj" -o "$($env:Build_ArtifactStagingDirectory)\scripts.sql" -i
+dotnet ef migrations script -p "Sopheon.CloudNative.Environments.Domain\Sopheon.CloudNative.Environments.Domain.csproj" -o "$($env:Build_ArtifactStagingDirectory)\scripts.sql" -i
 
 # Zip/Archive Scripts 
 # Write-Host "Zipping Artfacts for Environment Management...";
