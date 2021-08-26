8af789b867c7d53938319b52a7628554dfde522d
Ryan Weissman <ryan.weissman@sopheon.com>
2021-08-26 06:41:05 -0600

    cloud-1558 rename sln and project

---
+++ source/Sopheon.CloudNative.Environments/Sopheon.CloudNative.Environments.Domain/Data/EnvironmentContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Environment = Sopheon.CloudNative.Environments.Domain.Models.Environment;
+
+
+namespace Sopheon.CloudNative.Environments.Domain.Data
+{
+    class EnvironmentContext : DbContext
+    {
+        public EnvironmentContext()
+        {
+        }
+        public EnvironmentContext(DbContextOptions<EnvironmentContext> options) : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer();
+            }
+        }
+        public DbSet<Environment> Environments
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ source/Sopheon.CloudNative.Environments/Sopheon.CloudNative.Environments.Domain/Models/Environment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Sopheon.CloudNative.Environments.Domain.Models
+{
+    public class Environment
+    {
+        public int EnvironmentID
+        {
+            get;
+            set;
+        }
+
+        [MaxLength(100)]
+        public string Name
+        {
+            get;
+            set;
+        }
+    }
+}
