59a79997bf1c09ceafe15bca52a7976edc0934a3
Cal Hueber <Cal.Hueber@sopheon.com>
2021-08-26 13:12:45 -0600

    commented out items for later usage as not needed right now

--- source/Sopheon.CloudNative.EnvironmentAdmin/build/Build.ps1
+++ source/Sopheon.CloudNative.EnvironmentAdmin/build/Build.ps1
@@ -4,10 +4,12 @@ Copy-Item -Path "$($env:System_DefaultWorkingDirectory)/source/Sopheon.CloudNati
 
 Set-Location -Path "$($env:System_DefaultWorkingDirectory)/source/Sopheon.CloudNative.EnvironmentAdmin";
 
+#mkdir PublishOutput
+
 dotnet ef migrations script -p "Sopheon.CloudNative.EnvironmentAdmin.Data\Sopheon.CloudNative.EnvironmentAdmin.Data.csproj" -o "$($env:Build_ArtifactStagingDirectory)\scripts.sql" -i
 
 # Zip/Archive Scripts 
-Write-Host "Zipping Artfacts for Environment Management...";
-& $ZipUtil "a" "-tzip" "$($env:Build_ArtifactStagingDirectory)/EnvironmentManagement_$($env:Build_BuildId)" "$($env:System_DefaultWorkingDirectory)/source/Sopheon.CloudNative.EnvironmentAdmin/PublishOutput/*" "-xr!build" "-xr!deploy";
+# Write-Host "Zipping Artfacts for Environment Management...";
+# & $ZipUtil "a" "-tzip" "$($env:Build_ArtifactStagingDirectory)/EnvironmentManagement_$($env:Build_BuildId)" "$($env:System_DefaultWorkingDirectory)/source/Sopheon.CloudNative.EnvironmentAdmin/PublishOutput/*" "-xr!build" "-xr!deploy";
 
-Write-Host "Zipping Complete!";
+# Write-Host "Zipping Complete!";
