de0c197dc7597d04e5e4cd31f4f9f371bbf7a1f1
Ryan Weissman <ryan.weissman@sopheon.com>
2021-09-01 11:21:58 -0600

    cloud-1617 rename Functions.Get to be more generic

--- source/Sopheon.CloudNative.Environments/Sopheon.CloudNative.Environments.Functions/Models/EnvironmentDTO.cs
+++ source/Sopheon.CloudNative.Environments/Sopheon.CloudNative.Environments.Functions/Models/EnvironmentDTO.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Sopheon.CloudNative.Environments.Functions.Get.Models
+namespace Sopheon.CloudNative.Environments.Functions.Models
 {
    public class EnvironmentDTO
    {
