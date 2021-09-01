417076a70c357a97cc238261400facc8707919d2
Cal Hueber <Cal.Hueber@sopheon.com>
2021-09-01 15:04:50 -0600

    added error handle for when dotnet commands fails

--- source/Sopheon.CloudNative.Environments/build/Build.ps1
+++ source/Sopheon.CloudNative.Environments/build/Build.ps1
@@ -1,5 +1,11 @@
 $ZipUtil = "C:\Program Files\7-Zip\7z.exe";
 
+function Check-LastExitCode {
+    if ($LASTEXITCODE -ne 0) {
+        throw "Last Exit Code was not 0 but instead: $($LASTEXITCODE)";
+    }
+}
+
 Copy-Item -Path "$($env:System_DefaultWorkingDirectory)\source\Sopheon.CloudNative.Environments\deploy\*" -Destination $env:Build_ArtifactStagingDirectory;
 
 Set-Location -Path "$($env:System_DefaultWorkingDirectory)\source\Sopheon.CloudNative.Environments";
@@ -7,8 +13,10 @@ Set-Location -Path "$($env:System_DefaultWorkingDirectory)\source\Sopheon.CloudN
 mkdir PublishOutput
 
 dotnet ef migrations script -p "Sopheon.CloudNative.Environments.Domain\Sopheon.CloudNative.Environments.Domain.csproj" -o "$($env:Build_ArtifactStagingDirectory)\scripts.sql" -i;
+Check-LastExitCode;
 
-dotnet publish "Sopheon.CloudNative.Environments.Functions\Sopheon.CloudNative.Environments.Functions.csproj" -o ".\PublishOutput\";
+dotnet publish "Sopheon.CloudNative.Environments.Functions\Sopheon.CloudNative.Environments.Functions.Get.csproj" -o ".\PublishOutput\";
+Check-LastExitCode;
 
 # Zip/Archive Scripts 
 Write-Host "Zipping Artfacts for Environment Management...";
