438d71b76465edbbe742a80a55cb6a2ddbf9ba58
Cal Hueber <Cal.Hueber@sopheon.com>
2021-09-13 14:42:22 -0600

    Testing script for unit tests into one coverage file

--- source/Sopheon.CloudNative.Environments/build/Build.ps1
+++ source/Sopheon.CloudNative.Environments/build/Build.ps1
@@ -6,8 +6,18 @@ Set-Location -Path "$($env:System_DefaultWorkingDirectory)\source\Sopheon.CloudN
 
 mkdir PublishOutput
 
+$OutputCoveragePath = "$($env:System_DefaultWorkingDirectory)\source\Sopheon.CloudNative.Environments\TestResults\coverage.json"
+
+
+$TestProjects = Get-Item -Path "$($env:System_DefaultWorkingDirectory)\**\*.UnitTests.csproj"
+
+Foreach($file in $TestProjects) {
+    dotnet test $file.FullName /p:CollectCoverage=true /p:CoverletOutput="$($env:System_DefaultWorkingDirectory)\source\Sopheon.CloudNative.Environments\TestResults\" /p:CoverletOutputFormat="json%2cCobertura" /p:MergeWith=$OutputCoveragePath /p:Logger=trx
+}
+
 dotnet ef migrations script -p "Sopheon.CloudNative.Environments.Data\Sopheon.CloudNative.Environments.Data.csproj" -o "$($env:Build_ArtifactStagingDirectory)\scripts.sql" -i;
 Check-LastExitCode;
+
 
 dotnet publish "Sopheon.CloudNative.Environments.Functions\Sopheon.CloudNative.Environments.Functions.csproj" -o ".\PublishOutput\";
 Check-LastExitCode;
