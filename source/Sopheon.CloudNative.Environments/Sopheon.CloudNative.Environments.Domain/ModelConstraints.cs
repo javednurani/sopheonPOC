f543c2ea6adb68e06542802feb020e6260290a95
Ryan Weissman <ryan.weissman@sopheon.com>
2021-09-20 13:06:12 -0600

    cloud-1772 - Add Resource entity

--- source/Sopheon.CloudNative.Environments/Sopheon.CloudNative.Environments.Domain/ModelConstraints.cs
+++ source/Sopheon.CloudNative.Environments/Sopheon.CloudNative.Environments.Domain/ModelConstraints.cs
@@ -4,6 +4,7 @@ namespace Sopheon.CloudNative.Environments.Domain
    public static class ModelConstraints
    {
       public static int NAME_LENGTH = 64;
-      public static int DESCRIPTION_LENGTH = 1000;
+      public static int DESCRIPTION_LENGTH = 1000;  //Should we change this to 1024?
+      public static int URI_LENGTH = 2048;
    }
 }
