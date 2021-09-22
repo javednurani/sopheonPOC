d254a05857b0381ed86f726f3681eec2945df8a0
Brian Foster <brian.foster@sopheon.com>
2021-09-22 17:08:37 -0600

    Cloud-1777 update interface description

--- source/Sopheon.CloudNative.Environments/Sopheon.CloudNative.Environments.Domain/Queries/IEnvironmentQueries.cs
+++ source/Sopheon.CloudNative.Environments/Sopheon.CloudNative.Environments.Domain/Queries/IEnvironmentQueries.cs
@@ -11,9 +11,9 @@ namespace Sopheon.CloudNative.Environments.Domain.Queries
       /// <summary>
       /// Returns URIs for all resources that have been allocated to environments for the business service dependency
       /// </summary>
-      /// <param name="businessServiceName">Business Service name/key, string</param>
-      /// <param name="dependencyName">Business Service Dependency name/key, string</param>
-      /// <returns>IEnumerable of Uri (string? Dtos?)</returns>
+      /// <param name="businessServiceName">Business Service name, string</param>
+      /// <param name="dependencyName">Business Service Dependency name, string</param>
+      /// <returns>URIs, IEnumerable of string</returns>
       Task<IEnumerable<string>> GetResourceUrisByBusinessServiceDependency(string businessServiceName, string dependencyName);
    }
 }
