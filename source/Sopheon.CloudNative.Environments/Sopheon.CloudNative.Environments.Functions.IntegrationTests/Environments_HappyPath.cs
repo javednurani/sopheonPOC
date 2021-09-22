dc023572131ab7aaf08055570d1df53adff46346
Zach Quinn <ZachQuinn@sopheon.com>
2021-09-22 09:56:02 -0600

    cloud-1780 openAPI client and test

---
+++ source/Sopheon.CloudNative.Environments/Sopheon.CloudNative.Environments.Functions.IntegrationTests/Environments_HappyPath.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Sopheon.CloudNative.Environments.Functions.IntegrationTests
+{
+   public class Environments_HappyPath
+   {
+
+      readonly Environments_OpenApiClient _sut = new Environments_OpenApiClient(new HttpClient());
+
+      [FunctionFact]
+      public async Task HappyPath_AllFunctions()
+      {
+         // create an environment
+         var createDto = new EnvironmentDto { Name = "ZachIntegrationTest", Description = "TestDescription", Owner = Guid.NewGuid() };
+         var createdEnv = await _sut.CreateEnvironmentAsync(createDto);
+
+         // update environment
+         createdEnv.Name = "ZachIntegrationTest_Updated";
+         await _sut.UpdateEnvironmentAsync(createdEnv.EnvironmentKey, createdEnv);
+
+         // delete environment
+         await _sut.DeleteEnvironmentAsync(createdEnv.EnvironmentKey);
+
+         // get evironments - verify deleted
+         var environments = await _sut.GetEnvironmentsAsync();
+         Assert.DoesNotContain(environments, e => e.EnvironmentKey == createdEnv.EnvironmentKey);
+      }
+   }
+}
