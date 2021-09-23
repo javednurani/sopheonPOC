e1f6f7cae36c73c5cbc24fc28608d4b94a2a59ca
Brian Foster <brian.foster@sopheon.com>
2021-09-23 11:41:21 -0600

    Cloud-1777-2 add RequiredStringValidator unit tests

---
+++ source/Sopheon.CloudNative.Environments/Sopheon.CloudNative.Environments.Functions.UnitTests/RequiredStringValidator_UnitTests.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation.TestHelper;
+using Sopheon.CloudNative.Environments.Functions.Validators;
+using Sopheon.CloudNative.Environments.Testing.Common;
+using Xunit;
+
+namespace Sopheon.CloudNative.Environments.Functions.UnitTests
+{
+   public class RequiredStringValidator_UnitTests
+   {
+      private RequiredStringValidator _sut = new RequiredStringValidator();
+
+      [Theory]
+      [InlineData("")]
+      [InlineData(" ")]
+      [InlineData("  ")]
+      public void FieldsEmpty_ReturnsCorrectValidationErrors(string requiredString)
+      {
+         TestValidationResult<string> result = _sut.TestValidate(requiredString);
+         result.ShouldHaveValidationErrorFor(x => x);
+      }
+
+      [Fact]
+      public void ValidEnvironment_Success()
+      {
+         string requiredString = Some.Random.String();
+
+         TestValidationResult<string> result = _sut.TestValidate(requiredString);
+
+         result.ShouldNotHaveAnyValidationErrors();
+      }
+   }
+}
