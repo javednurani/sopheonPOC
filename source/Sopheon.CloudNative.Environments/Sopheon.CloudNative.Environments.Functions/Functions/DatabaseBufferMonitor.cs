73045e7cf706a1d18cc0264d1a9d630db4ff591a
Brian Foster <brian.foster@sopheon.com>
2021-09-29 17:00:18 -0600

    Cloud-1857 authenticate with service principal

--- source/Sopheon.CloudNative.Environments/Sopheon.CloudNative.Environments.Functions/Functions/DatabaseBufferMonitor.cs
+++ source/Sopheon.CloudNative.Environments/Sopheon.CloudNative.Environments.Functions/Functions/DatabaseBufferMonitor.cs
@@ -1,5 +1,11 @@
 using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Management.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Authentication;
+using Microsoft.Azure.Management.Sql.Fluent;
 using Microsoft.Extensions.Logging;
 
 namespace Sopheon.CloudNative.Environments.Functions
@@ -11,10 +17,62 @@ namespace Sopheon.CloudNative.Environments.Functions
       private const string NCRONTAB_EVERY_MINUTE = "0 * * * * *";
 
       [Function(nameof(DatabaseBufferMonitor))]
-      public static void Run([TimerTrigger(NCRONTAB_EVERY_MINUTE)] TimerInfo myTimer, FunctionContext context)
+      public static async void Run([TimerTrigger(NCRONTAB_EVERY_MINUTE)] TimerInfo myTimer, FunctionContext context)
       {
          ILogger logger = context.GetLogger(nameof(DatabaseBufferMonitor));
-         logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+         logger.LogInformation($"{nameof(DatabaseBufferMonitor)} Timer trigger function executed at: {DateTime.Now}");
+
+         try
+         {
+            // TODO 9/30 https://github.com/Azure/azure-functions-dotnet-worker/issues/534
+
+            IAzure azure = AuthenticateWithAzureServicePrincipal(logger);
+            logger.LogInformation($"Authenticated!");
+
+            // check buffer capacity
+            // case 1 - single server, single elastic pool 
+            var sqlServer = await azure.SqlServers.GetByIdAsync("...");
+
+            // case 2 - single server, multiple elastic pools
+            // case 3 - multiple servers, multiple elastic pool 
+
+            IReadOnlyList<ISqlElasticPool> elasticPools = await azure.SqlServers.ElasticPools
+               .ListBySqlServerAsync("...", "...");
+
+            //var databases = await azure.SqlServers.Databases.ListBySqlServerAsync();
+
+            // return if satisfied
+
+            // calculate what to deploy?
+
+            // create deployment
+
+            // write ENV.Resources records?
+
+         }
+         catch (Exception ex)
+         {
+            logger.LogInformation($"{ex.GetType()} : {ex.Message}");
+         }
       }
+
+      private static IAzure AuthenticateWithAzureServicePrincipal(ILogger logger)
+      {
+         // authenticate with Service Principal credentials
+         logger.LogInformation("Fetching Service Principal credentials");
+         string clientId = Environment.GetEnvironmentVariable("AzSpClientId");
+         string clientSecret = Environment.GetEnvironmentVariable("AzSpClientSecret");
+         string tenantId = Environment.GetEnvironmentVariable("AzSpTenantId");
+
+         AzureCredentials credentials = SdkContext.AzureCredentialsFactory
+            .FromServicePrincipal(clientId, clientSecret, tenantId, environment: AzureEnvironment.AzureGlobalCloud);
+
+         logger.LogInformation($"Authenticating with Azure...");
+
+         return Microsoft.Azure.Management.Fluent.Azure
+            .Authenticate(credentials)
+            .WithSubscription("...");
+      }
+
    }
 }
