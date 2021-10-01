66339039ab2463f57d271bed63cb9b034149b4aa
Brian Foster <brian.foster@sopheon.com>
2021-10-01 11:13:40 -0600

    Cloud-1857 comments and environment variables

--- source/Sopheon.CloudNative.Environments/Sopheon.CloudNative.Environments.Functions/Functions/DatabaseBufferMonitor.cs
+++ source/Sopheon.CloudNative.Environments/Sopheon.CloudNative.Environments.Functions/Functions/DatabaseBufferMonitor.cs
@@ -23,8 +23,7 @@ namespace Sopheon.CloudNative.Environments.Functions
       private const string NCRONTAB_EVERY_DAY = "0 0 0 * * *";
       // known issue https://github.com/Azure/azure-functions-dotnet-worker/issues/534
 
-      // buffered database indicator
-      // TODO post Cloud-1474: ENV.Resources and ENV.EnvironmentResourceBindings could be used as buffer indicator, instead of Tags on Azure SQL Databases
+      // buffered database indicator using Azure Tags
       // TODO Cloud-1474: when we create a buffered DB, it should be tagged as CustomerProvisionedDatabase = NotAssigned
       private const string CUSTOMER_PROVISIONED_DATABASE_TAG_NAME = "CustomerProvisionedDatabase"; // Tag Name/key for Azure Resouce (Azure SQL database)
       private const string CUSTOMER_PROVISIONED_DATABASE_TAG_VALUE_INITIAL = "NotAssigned"; // databases with this Tag Value are part of buffer
@@ -46,14 +45,13 @@ namespace Sopheon.CloudNative.Environments.Functions
             IAzure azure = await AuthenticateWithAzureServicePrincipal(logger);
             logger.LogInformation($"Authenticated with Service Principal to Subscription: {azure.SubscriptionId}!");
 
-            // Azure resources
-            string SUBSCRIPTION_ID = Environment.GetEnvironmentVariable("SubscriptionId");
-            string RESOURCE_GROUP_NAME = Environment.GetEnvironmentVariable("ResourceGroupName");
-            string SQL_SERVER_NAME = Environment.GetEnvironmentVariable("SqlServerName");
+            string subscriptionId = Environment.GetEnvironmentVariable("AzSubscriptionId");
+            string resourceGroupName = Environment.GetEnvironmentVariable("AzResourceGroupName");
+            string sqlServerName = Environment.GetEnvironmentVariable("AzSqlServerName");
 
             #region BufferCapacity
             ISqlServer sqlServer = await azure.SqlServers
-                     .GetByIdAsync($"/subscriptions/{SUBSCRIPTION_ID}/resourceGroups/{RESOURCE_GROUP_NAME}/providers/Microsoft.Sql/servers/{SQL_SERVER_NAME}");
+                     .GetByIdAsync($"/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Sql/servers/{sqlServerName}");
 
             List<ISqlDatabase> notAssigned = new List<ISqlDatabase>();
             List<ISqlDatabase> assignedToCustomer = new List<ISqlDatabase>();
@@ -89,18 +87,31 @@ namespace Sopheon.CloudNative.Environments.Functions
             }
             #endregion // BufferCapacity
 
+            #region BufferCapacityAlternate
+
+            // track buffer capacity using ENV database tables ENV.Resources and ENV.EnvironmentResourcesBindings
+            // add records to ENV.Resources table as DatabaseBufferMonitor creates Azure SQL Databases
+            // search for buffer databases by finding ENV.Resources records that are of correct DomainResourceType and not referenced in ENV.EnvironmentResourceBindings
+            // OR, track 'Assigned to Customer' status on ENV.Resources table
+
+            #endregion // BufferCapacityAlternate
+
             #region ExistingDeployments
+
             // TODO: return on some deployment conditions - provisioningStatus / tags etc
-            IPagedCollection<IDeployment> deploymentsForResourceGroup = await azure.Deployments.ListByResourceGroupAsync(RESOURCE_GROUP_NAME);
+            IPagedCollection<IDeployment> deploymentsForResourceGroup = await azure.Deployments.ListByResourceGroupAsync(resourceGroupName);
+
             #endregion // ExistingDeployments
 
             #region CreateDeployment
+
             // TODO: calculate what to deploy?
             IReadOnlyList<ISqlElasticPool> elasticPools = await azure.SqlServers.ElasticPools
-               .ListBySqlServerAsync(RESOURCE_GROUP_NAME, SQL_SERVER_NAME);
+               .ListBySqlServerAsync(resourceGroupName, sqlServerName);
 
             // create deployment
             // https://docs.microsoft.com/en-us/azure/azure-resource-manager/
+
             #endregion // CreateDeployment
 
             // TODO: write ENV.Resources records?
