e9dd78335f00bf49cdf53a20b3c0ebfc56ce79fc
Ryan Weissman <ryan.weissman@sopheon.com>
2021-10-05 09:06:37 -0600

    cloud-1876 add IDatabaseBufferMonitorHelper

---
+++ source/Sopheon.CloudNative.Environments/Sopheon.CloudNative.Environments.Functions/Helpers/IDatabaseBufferMonitorHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sopheon.CloudNative.Environments.Functions.Helpers
+{
+   public interface IDatabaseBufferMonitorHelper
+   {
+      /// <summary>
+      /// Query azure to see the avaiable, unavailable, and provisining databases.
+      /// Check to see if unavailable + provisioning >= threshold value
+      /// </summary>
+      /// <returns>true, if the number of database available meets or exceeds the threshold</returns>
+      Task<bool> CheckHasDatabaseThreshold();
+   }
+}
