c445ddac686558d6760349c0caffc9d8c152e275
Zach Quinn <ZachQuinn@sopheon.com>
2021-10-06 15:00:37 -0600

    cloud-1885 deployment skeleton and unit tests

--- source/Sopheon.CloudNative.Environments/Sopheon.CloudNative.Environments.Functions.UnitTests/Helpers/DatabaseBufferMonitorHelper_CheckHasDatabaseThreshold_UnitTests.cs
+++ source/Sopheon.CloudNative.Environments/Sopheon.CloudNative.Environments.Functions.UnitTests/Helpers/DatabaseBufferMonitorHelper_CheckHasDatabaseThreshold_UnitTests.cs
@@ -1,34 +1,77 @@
-﻿using Microsoft.Azure.Management.Fluent;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.Management.Fluent;
 using Microsoft.Azure.Management.ResourceManager.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Deployment.Definition;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Models;
 using Microsoft.Azure.Management.Sql.Fluent;
 using Microsoft.Extensions.Logging;
 using Moq;
 using Sopheon.CloudNative.Environments.Functions.Helpers;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace Sopheon.CloudNative.Environments.Functions.UnitTests.Helpers
 {
    public class DatabaseBufferMonitorHelper_CheckHasDatabaseThreshold_UnitTests
    {
+      private DatabaseBufferMonitorHelper _sut;
+      private Mock<ILogger<DatabaseBufferMonitorHelper>> _logger;
+      private Mock<IAzure> _azure;
+
+      public DatabaseBufferMonitorHelper_CheckHasDatabaseThreshold_UnitTests()
+      {
+         _logger = new Mock<ILogger<DatabaseBufferMonitorHelper>>();
+         _azure = new Mock<IAzure>();
+
+         _sut = new DatabaseBufferMonitorHelper(_logger.Object, _azure.Object);
+      }
+
       [Fact]
-      public async void DatabaseBufferMonitorHelper_CheckHasDatabaseThreshold_HappyPath()
+      public async void CheckHasDatabaseThreshold_EnoughUnassignedDatabasesExist_DeploymentIsNotCreated()
       {
          // Arrange
-         Mock<ILogger<DatabaseBufferMonitorHelper>> logger = new Mock<ILogger<DatabaseBufferMonitorHelper>>();
-         Mock<IAzure> azure = new Mock<IAzure>();
-         Mock<ISqlServers> mockSqlServers= new Mock<ISqlServers>();
+
+         // 5 unassigned databases means no need to run deployment
+         SetupMockDatabases(5);
+
+         Mock<IWithCreate> deploymentMock = SetupMockDeployment();
+
+         // Act
+         _ = await _sut.CheckHasDatabaseThreshold();
+
+         // Assert
+         deploymentMock.Verify(wc => wc.Create(), Times.Never, "Should not have created deployment!");
+      }
+
+      [Fact]
+      public async void CheckHasDatabaseThreshold_NotEnoughUnassignedDatabasesExist_DeploymentIsCreated()
+      {
+         // Arrange
+
+         // 4 unassigned databases means we need to deploymore databases
+         SetupMockDatabases(4);
+
+         Mock<IWithCreate> deploymentMock = SetupMockDeployment();
+
+         // Act
+         _ = await _sut.CheckHasDatabaseThreshold();
+
+         // Assert
+         deploymentMock.Verify(wc => wc.Create(), Times.Once, "Should have created deployment!");
+      }
+
+      private void SetupMockDatabases(int numUnassignedDatabases)
+      {
+         Mock<ISqlServers> mockSqlServers = new Mock<ISqlServers>();
          Mock<ISqlDatabaseOperations> mockDbOperations = new Mock<ISqlDatabaseOperations>();
          Mock<ISqlDatabase> mockDb = new Mock<ISqlDatabase>();
          Mock<IReadOnlyDictionary<string, string>> mockDbTags = new Mock<IReadOnlyDictionary<string, string>>();
 
-         azure.Setup(a => a.SqlServers).Returns(mockSqlServers.Object);
-         azure.Setup(a => a.Deployments).Returns(new Mock<IDeployments>().Object);
+         List<ISqlDatabase> unassignedDatabases = new List<ISqlDatabase>(Enumerable.Repeat(mockDb.Object, numUnassignedDatabases));
+
+         _azure.Setup(a => a.SqlServers).Returns(mockSqlServers.Object);
          var expectedValue = "NotAssigned";
          mockDbTags.Setup(t => t.TryGetValue(It.IsAny<string>(), out expectedValue)).Returns(true);
          mockDb.Setup(db => db.Tags).Returns(mockDbTags.Object);
@@ -37,17 +80,27 @@ namespace Sopheon.CloudNative.Environments.Functions.UnitTests.Helpers
             .Returns(Task.FromResult(mockDb.Object));
          mockDbOperations
             .Setup(dbo => dbo.ListBySqlServerAsync(It.IsAny<ISqlServer>(), It.IsAny<CancellationToken>()))
-            .Returns(Task.FromResult<IReadOnlyList<ISqlDatabase>>(new List<ISqlDatabase> { mockDb.Object }.AsReadOnly())); // could add more dbs here to get inside exit early switch
+            .Returns(Task.FromResult<IReadOnlyList<ISqlDatabase>>(unassignedDatabases.AsReadOnly()));
          mockSqlServers.Setup(s => s.Databases).Returns(mockDbOperations.Object);
          mockSqlServers.Setup(s => s.ElasticPools).Returns(new Mock<ISqlElasticPoolOperations>().Object);
+      }
 
-         DatabaseBufferMonitorHelper sut = new DatabaseBufferMonitorHelper(logger.Object, azure.Object);
-         
-         // Act
-         bool result = await sut.CheckHasDatabaseThreshold();
+      private Mock<IWithCreate> SetupMockDeployment()
+      {
+         Mock<IDeployments> mockDeployments = new Mock<IDeployments>();
+         _azure.Setup(a => a.Deployments).Returns(mockDeployments.Object);
+         Mock<IBlank> mockBlank = new Mock<IBlank>();
+         mockDeployments.Setup(d => d.Define(It.IsAny<string>())).Returns(mockBlank.Object);
+         Mock<IWithTemplate> mockWithTemplate = new Mock<IWithTemplate>();
+         mockBlank.Setup(b => b.WithExistingResourceGroup(It.IsAny<string>())).Returns(mockWithTemplate.Object);
+         Mock<IWithParameters> mockWithParameters = new Mock<IWithParameters>();
+         mockWithTemplate.Setup(wt => wt.WithTemplateLink(It.IsAny<string>(), It.IsAny<string>())).Returns(mockWithParameters.Object);
+         Mock<IWithMode> mockWithMode = new Mock<IWithMode>();
+         mockWithParameters.Setup(wp => wp.WithParameters(It.IsAny<object>())).Returns(mockWithMode.Object);
+         Mock<IWithCreate> mockWithCreate = new Mock<IWithCreate>();
+         mockWithMode.Setup(wm => wm.WithMode(It.IsAny<DeploymentMode>())).Returns(mockWithCreate.Object);
 
-         // Assert
-         Assert.True(result);
+         return mockWithCreate;
       }
    }
 }
