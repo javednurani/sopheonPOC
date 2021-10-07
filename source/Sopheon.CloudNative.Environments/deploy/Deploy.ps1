06575ae9d43fbeee137355e2c160f16b996133a9
Cal Hueber <Cal.Hueber@sopheon.com>
2021-10-07 11:30:36 -0600

    put in 0 for query timeout to have infinite timeout

--- source/Sopheon.CloudNative.Environments/deploy/Deploy.ps1
+++ source/Sopheon.CloudNative.Environments/deploy/Deploy.ps1
@@ -15,7 +15,7 @@ Write-Host "Running Sql Server Script for Environment Management";
 $FirewallCreation = az sql server firewall-rule create --resource-group $ResourceGroup --server $ResourceGroup.ToLower() --name DeployMachine --start-ip-address 50.200.9.230 --end-ip-address 50.200.9.230;
 Write-Host "Firewall Rule created";
 Write-Host "Running EF Sql Script on Sql Server Database: $($ResourceGroup)...";
-Invoke-Sqlcmd -ServerInstance "$($ResourceGroup.ToLower()).database.windows.net" -Database "EnvironmentManagement" -UserName "sopheon" -Password $SqlAdminEnigma -InputFile "_StratusEnvironmentManagement\EnvironmentManagement\scripts.sql";
+Invoke-Sqlcmd -ServerInstance "$($ResourceGroup.ToLower()).database.windows.net" -Database "EnvironmentManagement" -UserName "sopheon" -Password $SqlAdminEnigma -InputFile "_StratusEnvironmentManagement\EnvironmentManagement\scripts.sql" -QueryTimeout 0;
 Write-Host "Complete!";
 Write-Host "Removing Firewall Rule";
 az sql server firewall-rule delete --name DeployMachine --resource-group $ResourceGroup --server $ResourceGroup.ToLower();
