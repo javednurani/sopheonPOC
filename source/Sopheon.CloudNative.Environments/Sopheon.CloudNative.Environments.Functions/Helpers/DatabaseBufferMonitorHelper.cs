4807ffe6fac3619c5defef9af2d4c85c36b1138e
Ryan Weissman <ryan.weissman@sopheon.com>
2021-10-07 13:04:33 -0600

    cloud-1900 add deployment name for database buffer monitor function

--- source/Sopheon.CloudNative.Environments/Sopheon.CloudNative.Environments.Functions/Helpers/DatabaseBufferMonitorHelper.cs
+++ source/Sopheon.CloudNative.Environments/Sopheon.CloudNative.Environments.Functions/Helpers/DatabaseBufferMonitorHelper.cs
@@ -106,7 +106,7 @@ namespace Sopheon.CloudNative.Environments.Functions.Helpers
 
          // https://github.com/Azure-Samples/resources-dotnet-deploy-using-arm-template
          IDeployment deployment = _azure.Deployments
-            .Define(name: null)
+            .Define(name: nameof(DatabaseBufferMonitor) + "Deployment_" + DateTime.Now.ToString())
             .WithExistingResourceGroup(groupName: null)
             .WithTemplateLink(uri: null, contentVersion: null)
             .WithParameters(parameters: default(object))
