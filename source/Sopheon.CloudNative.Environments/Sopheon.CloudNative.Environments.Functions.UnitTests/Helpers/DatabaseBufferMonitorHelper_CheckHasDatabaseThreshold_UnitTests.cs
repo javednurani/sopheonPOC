840a56e8b71cd56dca281fbc2fd39ea1915a2bc0
Ryan Weissman <ryan.weissman@sopheon.com>
2021-10-07 13:43:48 -0600

    cloud-1899 remove unused using

--- source/Sopheon.CloudNative.Environments/Sopheon.CloudNative.Environments.Functions.UnitTests/Helpers/DatabaseBufferMonitorHelper_CheckHasDatabaseThreshold_UnitTests.cs
+++ source/Sopheon.CloudNative.Environments/Sopheon.CloudNative.Environments.Functions.UnitTests/Helpers/DatabaseBufferMonitorHelper_CheckHasDatabaseThreshold_UnitTests.cs
@@ -10,7 +10,6 @@ using Microsoft.Azure.Management.Sql.Fluent;
 using Microsoft.Extensions.Logging;
 using Moq;
 using Sopheon.CloudNative.Environments.Functions.Helpers;
-using Sopheon.CloudNative.Environments.Testing.Common;
 using Xunit;
 
 namespace Sopheon.CloudNative.Environments.Functions.UnitTests.Helpers
