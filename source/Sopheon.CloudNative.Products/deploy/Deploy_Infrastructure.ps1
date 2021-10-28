ada4cf26a6b5191a7f85d0e2922dc75989fc4a5b
Cal Hueber <Cal.Hueber@sopheon.com>
2021-10-28 07:49:43 -0600

    Clean up and start for deploy for products

--- source/Sopheon.CloudNative.Products/deploy/Deploy_Infrastructure.ps1
+++ source/Sopheon.CloudNative.Products/deploy/Deploy_Infrastructure.ps1
@@ -9,18 +9,11 @@ if ($env:AzureEnvironment -eq "Prod"){
     $azureKeyVault = "Prod-Cloud-DevOps"
 }
 
-$SqlAdminEnigma = (az keyvault secret show --vault-name $azureKeyVault --name "SqlServerAdminEnigma" --query value).Replace('"', '');
-
-
 $DeploymentName = "ADO-Deployment";
 
 $ResourceGroupValue = "Stratus-$($Environment)";
-$WebServerFarmName = "ASP-$($ResourceGroupValue)";
-$SqlServerNameValue = $ResourceGroupValue;
-$SqlServerPoolName = "$($ResourceGroupValue)-Pool";
-$FunctionAppName = $ResourceGroupValue.ToLower();
-$SqlServerDatabaseNameValue = "EnvironmentManagement";
-$FunctionAppStorageAccountName = "stratus$($Environment.ToLower())envfuncapp"
+$WebApiAppServiceName = $ResourceGroupValue.ToLower();
+$WebApiAppStorageAccountName = "stratus$($Environment.ToLower())webapiapp"
 $AppInsightsName = $ResourceGroupValue;
 
 $MasterTemplate = "$($PSScriptRoot)\Master_Template.bicep";
@@ -28,24 +21,20 @@ $MasterParametersTemplate = "$($PSScriptRoot)\Master_Template_Parameters.json";
 
 
 Write-Host "Replacing tokens on Master Template...";
-$masterTemplateContent = Get-Content $MasterTemplate -raw;
-$masterTemplateContent = $masterTemplateContent.Replace('^SqlServerName^', $SqlServerNameValue).Replace('^SqlServerDatabaseName^', $SqlServerDatabaseNameValue);
-$masterTemplateContent = $masterTemplateContent.Replace('^SqlElasticPoolName^', $SqlServerPoolName).Replace('^EnvironmentFunctionAppName^', $FunctionAppName);
-$masterTemplateContent = $masterTemplateContent.Replace('^AppInsightsName^', $AppInsightsName).Replace('^EnvironmentFunctionStorageAccountName^', $FunctionAppStorageAccountName);
-$masterTemplateContent = $masterTemplateContent.Replace('^SqlAdminEngima^', $SqlAdminEnigma).Replace('^WebServerFarmName^', $WebServerFarmName);
+$masterTemplateContent = $masterTemplateContent.Replace('^AppInsightsName^', $AppInsightsName).Replace('^WebApiProductsAppName^', $WebApiAppServiceName);
+$masterTemplateContent = $masterTemplateContent.Replace('^WebApiProductsStorageAccountName^', $WebApiAppStorageAccountName).Replace('^Environment^', $Environment);
 Set-Content -Value $masterTemplateContent -Path $MasterTemplate;
 Write-Host "Complete!";
 
 Write-Host "Replacing tokens on Master Parameters Template...";
 $masterParametersContent = Get-Content $MasterParametersTemplate -raw;
-$masterParametersContent = $masterParametersContent.Replace('^SqlServerName^', $SqlServerNameValue).Replace('^SqlServerDatabaseName^', $SqlServerDatabaseNameValue);
-$masterParametersContent = $masterParametersContent.Replace("^SqlElasticPoolName^", $SqlServerPoolName).Replace('^EnvironmentFunctionAppName^', $FunctionAppName);
-$masterParametersContent = $masterParametersContent.Replace('^AppInsightsName^', $AppInsightsName).Replace('^EnvironmentFunctionStorageAccountName^', $FunctionAppStorageAccountName);
-$masterParametersContent = $masterParametersContent.Replace('^SqlAdminEngima^', $SqlAdminEnigma).Replace('^WebServerFarmName^', $WebServerFarmName);
+$masterParametersContent = $masterParametersContent.Replace('^AppInsightsName^', $AppInsightsName).Replace('^WebApiProductsAppName^', $WebApiAppServiceName);
+$masterParametersContent = $masterParametersContent.Replace('^WebApiProductsStorageAccountName^', $WebApiAppStorageAccountName).Replace('^Environment^', $Environment);
 Set-Content -Value $masterParametersContent -Path $MasterParametersTemplate;
 Write-Host "Complete!";
 
-& "$($env:System_DefaultWorkingDirectory)\_TokenConfigurationManagement\TokenConfigManagement\TokenReplacer.exe" replace -c _StratusEnvironmentManagement\EnvironmentManagement\Environments_Configuration.json -f "$PSScriptRoot\*"  -e $Environment
+# TODO: Add in tokens for later configuration
+#& "$($env:System_DefaultWorkingDirectory)\_TokenConfigurationManagement\TokenConfigManagement\TokenReplacer.exe" replace -c _ProductManagement\ProductManagement\Product_Management_Configuration.json -f "$PSScriptRoot\*"  -e $Environment
 
 Write-Host "Deploying Storage Account Template to Resource Group: $($ResourceGroupValue)";
 # Creates a deployment for the given resource group and template.json
@@ -54,15 +43,9 @@ $GroupExists = az group exists --name $ResourceGroupValue;
 if('false' -eq $GroupExists)
 {
     Write-Host "Resource Group does not exist; Creating group: $($ResourceGroupValue)";
-    az group create --location 'westus' --name $ResourceGroupValue --tags 'Environment Type=Development' 'Owner=Cal' 'Review Date=12-30-21' --query "properties.provisioningState";
+    az group create --location 'westus' --name $ResourceGroupValue --tags 'Environment Type=Development' 'Owner=Cal' 'Review Date=6-30-22' --query "properties.provisioningState";
 }
 
 Write-Host "Deploying Master Template...";
-$MasterTemplateDeploy = az deployment group create --resource-group $ResourceGroupValue --template-file $MasterTemplate --parameters $MasterParametersTemplate --name "$($DeploymentName)-MasterDeploy-EnvironmentManagement" --query "properties.provisioningState";
+$MasterTemplateDeploy = az deployment group create --resource-group $ResourceGroupValue --template-file $MasterTemplate --parameters $MasterParametersTemplate --name "$($DeploymentName)-MasterDeploy-ProductManagement" --query "properties.provisioningState";
 Write-Host "Master Template Deployment: $($MasterTemplateDeploy)";
-
-$environmentManagementConnectionString = (az sql db show-connection-string --client ado.net --server "$($ResourceGroupValue.ToLower())" --name $SqlServerDatabaseNameValue).Replace('"', '');
-
-$environmentManagementConnectionString = $environmentManagementConnectionString.Replace('<username>', 'sopheon').Replace('<password>', $SqlAdminEnigma);
-
-$connectionString = az webapp config connection-string set --resource-group $ResourceGroupValue --name $ResourceGroupValue.ToLower() -t SQLServer --settings EnvironmentsSqlConnectionString=$environmentManagementConnectionString;
--- source/Sopheon.CloudNative.Products/deploy/Master_Template.bicep
+++ source/Sopheon.CloudNative.Products/deploy/Master_Template.bicep
@@ -1,7 +1,7 @@
 @description('The name of the Environment Function App')
-param webApiProductsApp_Name string = '^WebApiProductsAppName^'
+param webApiProductsAppName string = '^WebApiProductsAppName^'
 
-param webApiProductsAppStorage_name string = '^WebApiProductsStorageAccountName^'
+param webApiProductsAppStorageName string = '^WebApiProductsStorageAccountName^'
 
 param appInsightsName string = '^AppInsightsName^'
 
@@ -14,10 +14,10 @@ module EnvironmentFunction 'WebApi_App_Service.bicep' = {
   name: 'Function-App-Deployment'
   params: {
     location: location
-    storageAccountName: webApiProductsAppStorage_name
+    storageAccountName: webApiProductsAppStorageName
     env: env
     appInsightsName: appInsightsName
-    webAppName: webApiProductsApp_Name
+    webAppName: webApiProductsAppName
  }
 }
 
