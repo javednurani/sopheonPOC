8630dff03f58626840432f38ae3d8d1abaf02820
Brian Foster <brian.foster@sopheon.com>
2021-10-29 11:14:10 -0600

    Cloud-1976 code review feedback

--- source/Sopheon.CloudNative.Environments/Sopheon.CloudNative.Environments.Domain/Infrastructure/EnumExtensions.cs
+++ source/Sopheon.CloudNative.Environments/Sopheon.CloudNative.Environments.Domain/Infrastructure/EnumExtensions.cs
@@ -8,8 +8,8 @@ namespace Sopheon.CloudNative.Environments.Domain.Infrastructure
       public static TAttribute GetAttribute<TAttribute>(this Enum value)
         where TAttribute : Attribute
       {
-         var type = value.GetType();
-         var name = Enum.GetName(type, value);
+         Type type = value.GetType();
+         string name = Enum.GetName(type, value);
          return type.GetField(name)
              .GetCustomAttribute<TAttribute>();
       }
