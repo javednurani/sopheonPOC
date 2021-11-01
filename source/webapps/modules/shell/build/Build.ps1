32f9f35d45f5aba76da686d35d1f2d14600a447a
Cal Hueber <Cal.Hueber@sopheon.com>
2021-11-01 12:54:01 -0600

    updates on build pathing

--- source/webapps/modules/shell/build/Build.ps1
+++ source/webapps/modules/shell/build/Build.ps1
@@ -3,33 +3,33 @@ Import-Module "$($env:System_DefaultWorkingDirectory)\DevOps\PowerShell\CloudNat
 Copy-Item -Path "$($env:System_DefaultWorkingDirectory)\source\webapps\modules\shell\deploy\*" -Destination "$($env:Build_ArtifactStagingDirectory)";
 
 # Set location to packages shell-api
-Set-Location -Path "$($env:System_DefaultWorkingDirectory)\source\webapps\modules\packages\shell-api";
+Set-Location -Path "$($env:System_DefaultWorkingDirectory)\source\webapps\packages\shell-api";
 Write-Host "Location set for shared packages 'Shell-Api'";
-npm install
+npm install;
 
-Write-Host "Building package.json at Shell-Api location"
-npm run build
+Write-Host "Building package.json at Shell-Api location";
+npm run build;
 
 # Set location to packages shared-ui
-Set-Location -Path "$($env:System_DefaultWorkingDirectory)\source\webapps\modules\packages\shared-ui";
+Set-Location -Path "$($env:System_DefaultWorkingDirectory)\source\webapps\packages\shared-ui";
 Write-Host "Location set for shared packages 'shared-ui'";
-npm install
+npm install;
 
 Write-Host "Building package.json at shared-ui location"
-npm run build
+npm run build;
 
 # Set location to trial app
 Set-Location -Path "$($env:System_DefaultWorkingDirectory)\source\webapps\modules\shell";
 Write-Host "Location set for shared packages 'shell'";
-npm install
+npm install;
 
-npm install jest-junit
+npm install jest-junit;
 
-Write-Host "Building package.json at trial location"
-npm run build
+Write-Host "Building package.json at trial location";
+npm run build;
 
-Write-Host "Testing trial location"
-npm run test -- --ci --reporters=jest-junit --reporters=default --coverage --coverageReporters=cobertura --watchAll=false
+Write-Host "Testing trial location";
+npm run test -- --ci --reporters=jest-junit --reporters=default --coverage --coverageReporters=cobertura --watchAll=false;
 
 # Zip/Archive Scripts 
 Write-Host "Zipping Artfacts for Shell Trial...";
