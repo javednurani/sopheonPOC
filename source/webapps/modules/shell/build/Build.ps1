7d2b924db63b43a06368a83e5c2d21f5bd278a39
Cal Hueber <Cal.Hueber@sopheon.com>
2021-11-02 17:34:35 -0600

    updated build.ps1 and tests pointing to new path

--- source/webapps/modules/shell/build/Build.ps1
+++ source/webapps/modules/shell/build/Build.ps1
@@ -6,35 +6,43 @@ Copy-Item -Path "$($env:System_DefaultWorkingDirectory)\source\webapps\modules\s
 Set-Location -Path "$($env:System_DefaultWorkingDirectory)\source\webapps\packages\shell-api";
 Write-Host "Location set for shared packages 'Shell-Api'";
 npm install;
+Check-LastExitCode;
 
 Write-Host "Building package.json at Shell-Api location";
 npm run build;
+Check-LastExitCode;
 
 # Set location to packages shared-ui
 Set-Location -Path "$($env:System_DefaultWorkingDirectory)\source\webapps\packages\shared-ui";
 Write-Host "Location set for shared packages 'shared-ui'";
 npm install;
+Check-LastExitCode;
 
 Write-Host "Building package.json at shared-ui location"
 npm run build;
+Check-LastExitCode;
 
 # Set location to trial app
 Set-Location -Path "$($env:System_DefaultWorkingDirectory)\source\webapps\modules\shell";
 Write-Host "Location set for shared packages 'shell'";
 npm install;
+Check-LastExitCode;
 
+# install jest-junit to get reporting from the tests
 npm install jest-junit;
+Check-LastExitCode;
 
 Write-Host "Building package.json at trial location";
 npm run build;
+Check-LastExitCode;
 
 Write-Host "Testing trial location";
 npm run test -- --ci --reporters=jest-junit --reporters=default --coverage --coverageReporters=cobertura --watchAll=false;
+Check-LastExitCode;
 
 # Zip/Archive Scripts 
 Write-Host "Zipping Artfacts for Shell Trial...";
 & $ZipUtil "a" "-tzip" "$($env:Build_ArtifactStagingDirectory)\Shell_$($env:Build_BuildId)" "$($env:System_DefaultWorkingDirectory)\source\webapps\modules\shell\dist\*" "-xr!build" "-xr!deploy";
 & $ZipUtil "a" "-tzip" "$($env:Build_ArtifactStagingDirectory)\MarketingPage_$($env:Build_BuildId)" "$($env:System_DefaultWorkingDirectory)\source\webapps\website\*" "-xr!build" "-xr!deploy";
-
 
 Write-Host "Zipping Complete!";
