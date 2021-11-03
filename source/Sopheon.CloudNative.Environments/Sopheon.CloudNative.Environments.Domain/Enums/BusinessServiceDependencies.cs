1ca5dfe3e3dcc29e759b03116a5adc3226383f67
Brian Foster <brian.foster@sopheon.com>
2021-11-03 16:38:14 -0600

    Cloud-2037 remove EnvironmentSeedData

--- source/Sopheon.CloudNative.Environments/Sopheon.CloudNative.Environments.Domain/Enums/BusinessServiceDependencies.cs
+++ source/Sopheon.CloudNative.Environments/Sopheon.CloudNative.Environments.Domain/Enums/BusinessServiceDependencies.cs
@@ -2,6 +2,9 @@
 
 namespace Sopheon.CloudNative.Environments.Domain.Enums
 {
+   /// <summary>
+   /// Do not change these values; they are used to generate seed data
+   /// </summary>
    public enum BusinessServiceDependencies
    {
       [BusinessService(BusinessServices.ProductManagement)]
--- source/Sopheon.CloudNative.Environments/Sopheon.CloudNative.Environments.Domain/Enums/BusinessServices.cs
+++ source/Sopheon.CloudNative.Environments/Sopheon.CloudNative.Environments.Domain/Enums/BusinessServices.cs
@@ -1,5 +1,8 @@
 ﻿namespace Sopheon.CloudNative.Environments.Domain.Enums
 {
+   /// <summary>
+   /// Do not change these values; they are used to generate seed data
+   /// </summary>
    public enum BusinessServices
    {
       ProductManagement = 1,
