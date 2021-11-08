b9a418a6bb1249a17815735b7fc66ecff140a618
Brian Foster <brian.foster@sopheon.com>
2021-11-08 09:15:29 -0700

    Cloud-2038 try remove ProductMgmt migration

--- source/Sopheon.CloudNative.Environments/Sopheon.CloudNative.Environments.Data/Migrations/EnvironmentContextModelSnapshot.cs
+++ source/Sopheon.CloudNative.Environments/Sopheon.CloudNative.Environments.Data/Migrations/EnvironmentContextModelSnapshot.cs
@@ -39,13 +39,6 @@ namespace Sopheon.CloudNative.Environments.Data.Migrations
                         .IsUnique();
 
                     b.ToTable("BusinessServices");
-
-                    b.HasData(
-                        new
-                        {
-                            Id = 1,
-                            Name = "ProductManagement"
-                        });
                 });
 
             modelBuilder.Entity("Sopheon.CloudNative.Environments.Domain.Models.BusinessServiceDependency", b =>
@@ -75,15 +68,6 @@ namespace Sopheon.CloudNative.Environments.Data.Migrations
                         .IsUnique();
 
                     b.ToTable("BusinessServiceDependencies");
-
-                    b.HasData(
-                        new
-                        {
-                            Id = 1,
-                            BusinessServiceId = 1,
-                            DependencyName = "SqlDatabase",
-                            DomainResourceTypeId = 1
-                        });
                 });
 
             modelBuilder.Entity("Sopheon.CloudNative.Environments.Domain.Models.DomainResourceType", b =>
