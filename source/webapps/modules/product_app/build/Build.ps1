335fcd33370f4937ef0ce887032278cbedc6e3c7
Cal Hueber <Cal.Hueber@sopheon.com>
2021-11-08 09:50:06 -0700

    Updated build script

--- source/webapps/modules/product_app/build/Build.ps1
+++ source/webapps/modules/product_app/build/Build.ps1
@@ -1,6 +1,6 @@
 Import-Module "$($env:System_DefaultWorkingDirectory)\DevOps\PowerShell\CloudNative.Common.psm1";
 
-Copy-Item -Path "$($env:System_DefaultWorkingDirectory)\source\webapps\modules\app\deploy\*" -Destination "$($env:Build_ArtifactStagingDirectory)";
+Copy-Item -Path "$($env:System_DefaultWorkingDirectory)\source\webapps\modules\product_app\deploy\*" -Destination "$($env:Build_ArtifactStagingDirectory)";
 
 # Set location to packages shell-api
 Set-Location -Path "$($env:System_DefaultWorkingDirectory)\source\webapps\packages\shell-api";
@@ -19,7 +19,7 @@ Write-Host "Building package.json at shared-ui location"
 npm run build
 
 # Set location to trial app
-Set-Location -Path "$($env:System_DefaultWorkingDirectory)\source\webapps\modules\app";
+Set-Location -Path "$($env:System_DefaultWorkingDirectory)\source\webapps\modules\product_app";
 Write-Host "Location set for shared packages 'app'";
 npm ci
 
@@ -33,7 +33,7 @@ npm run test -- --ci --reporters=jest-junit --reporters=default --coverage --cov
 
 # Zip/Archive Scripts 
 Write-Host "Zipping Artfacts for App...";
-& $ZipUtil "a" "-tzip" "$($env:Build_ArtifactStagingDirectory)\App_$($env:Build_BuildId)" "$($env:System_DefaultWorkingDirectory)\source\webapps\modules\app\dist\*" "-xr!build" "-xr!deploy";
+& $ZipUtil "a" "-tzip" "$($env:Build_ArtifactStagingDirectory)\App_$($env:Build_BuildId)" "$($env:System_DefaultWorkingDirectory)\source\webapps\modules\product_app\dist\*" "-xr!build" "-xr!deploy";
 
 
 Write-Host "Zipping Complete!";
