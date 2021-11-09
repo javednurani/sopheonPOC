4fa49664b57400eeba50fa569ff6cabe9f399fc0
Brian Foster <brian.foster@sopheon.com>
2021-11-09 09:33:24 -0700

    Cloud-2128 add progress indicator

--- source/webapps/modules/product_app/src/onboardingInfo.tsx
+++ source/webapps/modules/product_app/src/onboardingInfo.tsx
@@ -1,4 +1,4 @@
-import { Label, PrimaryButton, Stack, TextField } from '@fluentui/react';
+import { Label, PrimaryButton, ProgressIndicator, Stack, TextField } from '@fluentui/react';
 import React from 'react';
 import { FormattedMessage, useIntl } from 'react-intl';
 
@@ -10,6 +10,9 @@ const headerStyle: React.CSSProperties = {
   marginTop: '20px',
   marginBottom: '20px',
   fontSize: '40px',
+};
+const progressBarStyles: React.CSSProperties = {
+  padding: '0vh 25vw',
 };
 
 const OnboardingInfo: React.FunctionComponent<OnboardingInfoProps> = ({ currentStep, nextStep }: OnboardingInfoProps) => {
@@ -47,6 +50,15 @@ const OnboardingInfo: React.FunctionComponent<OnboardingInfoProps> = ({ currentS
               text={formatMessage({ id: 'onboarding.getstarted' })}
               aria-label={formatMessage({ id: 'onboarding.getstarted' })}
               onClick={() => nextStep()}
+            />
+          </Stack.Item>
+          <Stack.Item align={'auto'} style={progressBarStyles}>
+            <ProgressIndicator
+              label={formatMessage({ id: 'onboarding.step3of3' })}
+              description={formatMessage({ id: 'onboarding.done' })}
+              ariaValueText={formatMessage({ id: 'onboarding.step3of3' })}
+              percentComplete={1}
+              barHeight={12}
             />
           </Stack.Item>
         </Stack>
