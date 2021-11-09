00092127284a54637e1bf2b028c039de45a716a6
Ryan Weissman <ryan.weissman@sopheon.com>
2021-11-09 09:43:13 -0700

    cloud-2017 update spacing/styles on app

--- source/webapps/modules/product_app/src/App.tsx
+++ source/webapps/modules/product_app/src/App.tsx
@@ -1,28 +1,18 @@
-import { Label, PrimaryButton } from '@fluentui/react';
+import { Label } from '@fluentui/react';
 import { AppProps } from '@sopheon/shell-api';
 import React from 'react';
-import { useIntl } from 'react-intl';
 
 import { AppDispatchProps, AppStateProps } from './AppContainer';
 import OnboardingInfo from './onboardingInfo';
 
 export type Props = AppProps<AppStateProps, AppDispatchProps>;
 
-const App: React.FunctionComponent<Props> = ({ currentStep, nextStep }: Props) => {
-  const { formatMessage } = useIntl();
-
-  return (
-    <div>
-      <Label>
-        <OnboardingInfo currentStep={currentStep} nextStep={nextStep} />
-        <PrimaryButton
-          text={currentStep === 2 ? formatMessage({ id: 'continue' }) : formatMessage({ id: 'getStarted' })}
-          aria-label={currentStep === 2 ? formatMessage({ id: 'continue' }) : formatMessage({ id: 'getStarted' })}
-          onClick={() => nextStep()}
-        />
-      </Label>
-    </div>
-  );
-};
+const App: React.FunctionComponent<Props> = ({ currentStep, nextStep }: Props) => (
+  <div>
+    <Label>
+      <OnboardingInfo currentStep={currentStep} nextStep={nextStep} />
+    </Label>
+  </div>
+);
 
 export default App;
