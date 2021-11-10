df703a2521814524d9bf191b48871b8102bc2bed
Brian Foster <brian.foster@sopheon.com>
2021-11-10 12:47:48 -0700

    Cloud-2150 onboardingService

---
+++ source/webapps/modules/product_app/src/onboardingService.ts
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+import { settings } from './settings';
+import { Product } from './types';
+
+// TODO Cloud-2148 remove hardcoded EnvironmentKey
+const ENVIRONMENT_KEY_STUB = '00000000-0000-0000-0000-000000000001';
+
+const API_URL_BASE: string = settings.ProductManagementApiUrlBase;
+const API_URL_PATH_CREATE_PRODUCT: string = settings.CreateProductUrlPath;
+const API_URL_PATH_UPDATE_PRODUCT: string = settings.UpdateProductUrlPath;
+
+export const createProduct: (product: Product) => Promise<Product> = async product => {
+  const createProductUrlWithEnvironment = `${API_URL_BASE}${API_URL_PATH_CREATE_PRODUCT}`
+    .replace(settings.TokenEnvironmentKey, ENVIRONMENT_KEY_STUB);
+  return await axios.post(createProductUrlWithEnvironment, product);
+};
+
+export const updateProduct: (product: Product) => Promise<Product> = async product => {
+  const updateProductUrlWithEnvironment = `${API_URL_BASE}${API_URL_PATH_UPDATE_PRODUCT}`
+    .replace(settings.TokenEnvironmentKey, ENVIRONMENT_KEY_STUB)
+    .replace(settings.TokenProductKey, product.Key);
+  return await axios.post(updateProductUrlWithEnvironment, product);
+};
+
