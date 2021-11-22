d609e5ebdf990adaab5f294b95b495f188e04130
Brian Foster <brian.foster@sopheon.com>
2021-11-22 15:00:53 -0700

    Cloud-2151 change token value

--- source/webapps/modules/product_app/src/settings.ts
+++ source/webapps/modules/product_app/src/settings.ts
@@ -2,8 +2,8 @@ import { PRODUCT_MANAGEMENT_API_BASE_URL } from '../../../devEnvSettings';
 
 const isDev = process.env.NODE_ENV === 'development';
 
-// INFO: &token& is meant to be replaced in Deploy CI
-const productManagementApiUrlBase = isDev ? PRODUCT_MANAGEMENT_API_BASE_URL : '&ProdMgmtUrlBaseToken&';
+// INFO: ^token^ is meant to be replaced in Deploy CI
+const productManagementApiUrlBase = isDev ? PRODUCT_MANAGEMENT_API_BASE_URL : '^ProdMgmtUrlBaseToken^';
 
 // INFO: {*token*} is meant to be replaced in React SPA
 const tokenEnvironmentKey = '{*environmentKey*}';
