d962cf0087398c0979219fc0c72e9f7892f926d1
Brian Foster <brian.foster@sopheon.com>
2021-11-22 15:28:33 -0700

    Cloud-2146 fix git merge

--- source/Sopheon.CloudNative.Products/Sopheon.CloudNative.Products.AspNetCore/Models/ProductDto.cs
+++ source/Sopheon.CloudNative.Products/Sopheon.CloudNative.Products.AspNetCore/Models/ProductDto.cs
@@ -2,74 +2,75 @@
 
 namespace Sopheon.CloudNative.Products.AspNetCore.Models
 {
-    public class ProductDto
-    {
-        public int Id { get; set; }
+   public class ProductDto
+   {
+      public int Id { get; set; }
 
-        public List<Int32AttributeValueDto> IntAttributeValues { get; set; }
+      public List<Int32AttributeValueDto> IntAttributeValues { get; set; }
 
-        public List<StringAttributeValueDto> StringAttributeValues { get; set; }
+      public List<StringAttributeValueDto> StringAttributeValues { get; set; }
 
-        public List<DecimalAttributeValueDto> DecimalAttributeValues { get; set; }
+      public List<DecimalAttributeValueDto> DecimalAttributeValues { get; set; }
 
-        public List<UtcDateTimeAttributeValueDto> UtcDateTimeAttributeValues { get; set; }
+      public List<UtcDateTimeAttributeValueDto> UtcDateTimeAttributeValues { get; set; }
 
-        public List<MoneyAttributeValueDto> MoneyAttributeValues { get; set; }
+      public List<MoneyAttributeValueDto> MoneyAttributeValues { get; set; }
 
-        public List<KeyPerformanceIndicatorDto> KeyPerformanceIndicators { get; set; }
+      public List<KeyPerformanceIndicatorDto> KeyPerformanceIndicators { get; set; }
 
-        public string Name { get; set; }
+      public string Name { get; set; }
 
-        public string Description { get; set; }
+      public string Description { get; set; }
 
-        //public int? StatusId { get; set; }
+      //public int? StatusId { get; set; }
 
-        //public Status Status { get; set; }
+      //public Status Status { get; set; }
 
-        public string Key { get; set; }
+      public string Key { get; set; }
 
-        public List<ProductGoalDto> Goals { get; set; }
+      public List<ProductGoalDto> Goals { get; set; }
 
-        //public List<ProductItem> Items { get; set; }
+      //public List<ProductItem> Items { get; set; }
 
-        //public List<FileAttachment> FileAttachments { get; set; }
+      //public List<FileAttachment> FileAttachments { get; set; }
 
-        //public List<UrlLink> UrlLinks { get; set; }
+      //public List<UrlLink> UrlLinks { get; set; }
 
-        //public List<Release> Releases { get; set; }
-    }
+      //public List<Release> Releases { get; set; }
+   }
 
-    public class ProductPatchDto
-    {
-        //public int Id { get; set; }
+   public class ProductPatchDto
+   {
+      //public int Id { get; set; }
 
-        public List<Int32AttributeValueDto> IntAttributeValues { get; set; }
+      public List<Int32AttributeValueDto> IntAttributeValues { get; set; }
 
-        //public StringAttributeValueDto[] StringAttributeValues { get; set; }
+      //public StringAttributeValueDto[] StringAttributeValues { get; set; }
 
-        public List<KeyPerformanceIndicatorDto> KeyPerformanceIndicators { get; set; }
+      public List<KeyPerformanceIndicatorDto> KeyPerformanceIndicators { get; set; }
 
-        //public DecimalAttributeValueDto[] DecimalAttributeValues { get; set; }
+      //public DecimalAttributeValueDto[] DecimalAttributeValues { get; set; }
 
-        //public UtcDateTimeAttributeValueDto[] UtcDateTimeAttributeValues { get; set; }
+      //public UtcDateTimeAttributeValueDto[] UtcDateTimeAttributeValues { get; set; }
 
-        //public MoneyAttributeValueDto[] MoneyAttributeValues { get; set; }
+      //public MoneyAttributeValueDto[] MoneyAttributeValues { get; set; }
 
-        public string Name { get; set; }
+      public string Name { get; set; }
 
-        public string Description { get; set; }
+      public string Description { get; set; }
 
-        //public int? StatusId { get; set; }
+      //public int? StatusId { get; set; }
 
-        //public Status Status { get; set; }
+      //public Status Status { get; set; }
 
-        //public string Key { get; set; }
+      //public string Key { get; set; }
 
-        public List<ProductGoalDto> Goals { get; set; }
+      public List<ProductGoalDto> Goals { get; set; }
 
-        //public List<ProductItem> Items { get; set; }
+      //public List<ProductItem> Items { get; set; }
 
-        //public List<FileAttachment> FileAttachments { get; set; }
+      //public List<FileAttachment> FileAttachments { get; set; }
+   }
 
    public class ProductPostDto
    {
