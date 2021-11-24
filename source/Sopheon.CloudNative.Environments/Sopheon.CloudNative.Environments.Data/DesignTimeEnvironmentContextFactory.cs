2b1268b1a529589e7529cb234ae74f3c625b73c0
Cal Hueber <Cal.Hueber@sopheon.com>
2021-11-24 09:38:02 -0700

    DevOps - Corrected setup for optionsBuilder.UseSqlServer on the context factory

--- source/Sopheon.CloudNative.Environments/Sopheon.CloudNative.Environments.Data/DesignTimeEnvironmentContextFactory.cs
+++ source/Sopheon.CloudNative.Environments/Sopheon.CloudNative.Environments.Data/DesignTimeEnvironmentContextFactory.cs
@@ -19,9 +19,12 @@ namespace Sopheon.CloudNative.Environments.Data
          // dotnet ef database update -- --connectionstring "Server=.;Database=TenantEnvironment1;Integrated Security=True"
          // dotnet ef database update -- --connectionstring "Server=.;Database=env;User Id=sa;Password=###; Trusted_Connection=False;MultipleActiveResultSets=true"
          string connectionString = args.Length >= 2 && args[0].Equals("--connectionstring", System.StringComparison.OrdinalIgnoreCase) ? args[1] : string.Empty;
-         optionsBuilder.UseSqlServer(connectionString);
+         optionsBuilder.UseSqlServer(connectionString,
+			b => b.MigrationsHistoryTable(
+				"DBInstallHistory",
+				EnvironmentContext.DEFAULT_SCHEMA));
 
-         return new EnvironmentContext(optionsBuilder.Options);
+			return new EnvironmentContext(optionsBuilder.Options);
       }
    }
 }
