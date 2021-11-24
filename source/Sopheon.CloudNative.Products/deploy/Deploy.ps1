64920a6d3b9e6f91f1b1da70681ebced8692a188
Cal Hueber <Cal.Hueber@sopheon.com>
2021-11-24 14:26:07 -0700

    adjusted deploy script for typos

--- source/Sopheon.CloudNative.Products/deploy/Deploy.ps1
+++ source/Sopheon.CloudNative.Products/deploy/Deploy.ps1
@@ -15,10 +15,10 @@ $WebApiAppServiceName = "stratus-productmanagement-$env:Environment";
 
 & $ZipUtil "x" "$($PSScriptRoot)/ProductManagement.zip" "-o$($ProductManagementPath)";
 
-& "$($env:System_DefaultWorkingDirectory)\_TokenConfigurationManagement\TokenConfigManagement\TokenReplacer.exe" replace -c _ProductMangement\ProductManagement\Product_Management_Configuration.json -f "$ProductManagementPath\*"  -e $env:Environment
+& "$($env:System_DefaultWorkingDirectory)\_TokenConfigurationManagement\TokenConfigManagement\TokenReplacer.exe" replace -c _ProductManagement\ProductManagement\Product_Management_Configuration.json -f "$ProductManagementPath\*"  -e $env:Environment
 
-& $ZipUtil "a" "-tzip" "$env:Build_ArtifactStagingDirectory\ProductManagement-Staged" $ProductManagementPath
+& $ZipUtil "a" "-tzip" "$env:Build_ArtifactStagingDirectory" $ProductManagementPath
 
 
 #upload webapp app
-az webapp deployment source config-zip --name $WebApiAppServiceName --resource-group $ResourceGroup --src "ProductManagement-Staged\ProductManagement.zip";
+az webapp deployment source config-zip --name $WebApiAppServiceName --resource-group $ResourceGroup --src "ProductManagement.zip";
