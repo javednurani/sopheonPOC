e41d94963a6ea7a74aa0528a3d8e03508dda40d5
Cal Hueber <Cal.Hueber@sopheon.com>
2021-11-30 12:07:37 -0700

    Final adjustment in the command structure

--- StartServices.ps1
+++ StartServices.ps1
@@ -1,20 +1,20 @@
 Push-Location "$($PSScriptRoot)\source\setupScripts"
 
 
-Start-Process powershell -ArgumentList { $host.UI.RawUI.WindowTitle = 'Environment Service'; & '.\StartEnvService.ps1'; };
+Start-Process powershell -ArgumentList ("-NoExit", { $host.UI.RawUI.WindowTitle = 'Environment Service'; & '.\StartEnvService.ps1'; });
 
-Start-Process powershell -ArgumentList { $host.ui.RawUI.WindowTitle = 'Product Service'; & '.\StartProductService.ps1' };
+Start-Process powershell -ArgumentList ("-NoExit", { $host.UI.RawUI.WindowTitle = 'Product Service'; & '.\StartProductService.ps1'; });
 
 Push-Location "..\webapps"
 
 Write-Output "Build NPM and run."
-pm run buildpkgs
+npm run buildpkgs
 Write-Output "..................";
-#npm run buildmods
+npm run buildmods
 Write-Output "............."
 npm install
 Write-Output "........"
-Start-Process powershell -ArgumentList { $host.ui.RawUI.WindowTitle = 'Website Server'; npm run start; Read-Host; };
+Start-Process powershell -ArgumentList ("-NoExit", { $host.UI.RawUI.WindowTitle = 'Website Server'; npm run start; });
 Write-Output "...."
 Write-Output "Done!"
 Write-Output "Services and react node server should be running in new windows..."
