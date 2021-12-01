4c030ef0b692dfa4207fa5472fc663c8459b883b
Zach Quinn <ZachQuinn@sopheon.com>
2021-11-30 22:14:46 -0700

    cloud-2290 initial dashboard layout

--- source/webapps/modules/product_app/src/Dashboard.tsx
+++ source/webapps/modules/product_app/src/Dashboard.tsx
@@ -1,7 +1,40 @@
+import { DefaultPalette, IStackItemStyles, IStackStyles, IStackTokens, Stack } from '@fluentui/react';
 import React from 'react';
 
 export interface IDashboardProps {}
 
-const Dashboard: React.FunctionComponent<IDashboardProps> = ({}: IDashboardProps) => <div>Dashboard Component</div>;
+const stackStyles: IStackStyles = {
+  root: {
+    //background: DefaultPalette.themeTertiary,
+    height: '100%',
+  },
+};
+const stackItemStyles: IStackItemStyles = {
+  root: {
+    alignItems: 'center',
+    background: 'white',
+    //color: DefaultPalette.white,
+    display: 'flex',
+    justifyContent: 'center',
+    border: '1px solid',
+    borderColor: 'black',
+  },
+};
+
+const stackTokens: IStackTokens = {
+  childrenGap: 5,
+  padding: 10,
+};
+
+const Dashboard: React.FunctionComponent<IDashboardProps> = ({}: IDashboardProps) => (
+  <Stack horizontal styles={stackStyles} tokens={stackTokens}>
+    <Stack.Item grow={4} styles={stackItemStyles}>
+      Main Left
+    </Stack.Item>
+    <Stack.Item grow={7} styles={stackItemStyles}>
+      Main Right
+    </Stack.Item>
+  </Stack>
+);
 
 export default Dashboard;
