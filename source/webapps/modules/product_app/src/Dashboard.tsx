e87af12b7c89fc44ea9deb2b58ff9b316364df0a
Zach Quinn <ZachQuinn@sopheon.com>
2021-11-30 22:33:12 -0700

    cloud-2290 stack colors use theme

--- source/webapps/modules/product_app/src/Dashboard.tsx
+++ source/webapps/modules/product_app/src/Dashboard.tsx
@@ -1,40 +1,45 @@
-import { DefaultPalette, IStackItemStyles, IStackStyles, IStackTokens, Stack } from '@fluentui/react';
+import { IStackItemStyles, IStackStyles, IStackTokens, Stack } from '@fluentui/react';
+import { useTheme } from '@fluentui/react-theme-provider';
 import React from 'react';
 
 export interface IDashboardProps {}
-
-const stackStyles: IStackStyles = {
-  root: {
-    //background: DefaultPalette.themeTertiary,
-    height: '100%',
-  },
-};
-const stackItemStyles: IStackItemStyles = {
-  root: {
-    alignItems: 'center',
-    background: 'white',
-    //color: DefaultPalette.white,
-    display: 'flex',
-    justifyContent: 'center',
-    border: '1px solid',
-    borderColor: 'black',
-  },
-};
 
 const stackTokens: IStackTokens = {
   childrenGap: 5,
   padding: 10,
 };
 
-const Dashboard: React.FunctionComponent<IDashboardProps> = ({}: IDashboardProps) => (
-  <Stack horizontal styles={stackStyles} tokens={stackTokens}>
-    <Stack.Item grow={4} styles={stackItemStyles}>
-      Main Left
-    </Stack.Item>
-    <Stack.Item grow={7} styles={stackItemStyles}>
-      Main Right
-    </Stack.Item>
-  </Stack>
-);
+const Dashboard: React.FunctionComponent<IDashboardProps> = ({}: IDashboardProps) => {
+  const theme = useTheme();
+
+  const stackItemStyles: IStackItemStyles = {
+    root: {
+      alignItems: 'center',
+      background: theme.semanticColors.bodyBackground, // TODO: why needed?
+      display: 'flex',
+      justifyContent: 'center',
+      border: '1px solid',
+      borderColor: theme.semanticColors.bodyText,
+    },
+  };
+
+  const stackStyles: IStackStyles = {
+    root: {
+      background: theme.semanticColors.bodyBackground, // TODO: why needed?
+      height: '100%',
+    },
+  };
+
+  return (
+    <Stack horizontal styles={stackStyles} tokens={stackTokens}>
+      <Stack.Item grow={4} styles={stackItemStyles}>
+        Main Left
+      </Stack.Item>
+      <Stack.Item grow={7} styles={stackItemStyles}>
+        Main Right
+      </Stack.Item>
+    </Stack>
+  );
+};
 
 export default Dashboard;
