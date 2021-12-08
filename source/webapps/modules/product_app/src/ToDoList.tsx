448532ae3d49a9915477816dc19640a46c64c56b
Zach Quinn <ZachQuinn@sopheon.com>
2021-12-07 18:36:31 -0700

    cloud-2375 font sizes

--- source/webapps/modules/product_app/src/ToDoList.tsx
+++ source/webapps/modules/product_app/src/ToDoList.tsx
@@ -1,4 +1,5 @@
-import { FontIcon, mergeStyles, Stack, StackItem } from 'office-ui-fabric-react';
+import { ITextProps, Text } from '@fluentui/react/lib/Text';
+import { FontIcon, FontSizes, mergeStyles, Stack, StackItem } from 'office-ui-fabric-react';
 import React from 'react';
 
 export interface IToDoListProps {}
@@ -15,27 +16,41 @@ const headingLeftStyle: React.CSSProperties = {
 };
 
 const contentDivStyle: React.CSSProperties = {
-  marginTop: '24px',
+  margin: '24px',
+  marginLeft: '72px',
+  marginRight: '72px',
 };
 
 const iconClass = mergeStyles({
   marginLeft: '7px',
 });
 
+const filterSortIconClass = mergeStyles({
+  marginLeft: '7px',
+  verticalAlign: 'bottom',
+});
+
 const ToDoList: React.FunctionComponent<IToDoListProps> = ({}: IToDoListProps) => (
   <div style={mainDivStyle}>
     <Stack horizontal>
       <Stack.Item grow style={headingLeftStyle}>
-        To Do <FontIcon iconName="CirclePlus" className={iconClass} />
+        <Text variant="xxLarge">To Do</Text>
+        <Text variant="xLarge">
+          <FontIcon iconName="CirclePlus" className={iconClass} />
+        </Text>
       </Stack.Item>
       <Stack.Item>
-        <FontIcon iconName="Filter" className={iconClass} />
-        <FontIcon iconName="Sort" className={iconClass} />
+        <Text variant="xLarge">
+          <FontIcon iconName="Filter" className={filterSortIconClass} />
+          <FontIcon iconName="Sort" className={filterSortIconClass} />
+        </Text>
       </Stack.Item>
     </Stack>
     <hr />
     <div style={contentDivStyle}>
-      You don't have any tasks yet. Click <FontIcon iconName="CirclePlus" /> above to add one.
+      <Text variant="xLarge">
+        You don't have any tasks yet. Click <FontIcon iconName="CirclePlus" /> above to add one.
+      </Text>
     </div>
   </div>
 );
