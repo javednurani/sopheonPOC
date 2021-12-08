866a851e144f38fb9417242483427ec57e1c881b
Brian Foster <brian.foster@sopheon.com>
2021-12-08 11:02:24 -0700

    Cloud-2390 split ProductItem, remove Migration

--- source/Sopheon.CloudNative.Products/Sopheon.CloudNative.Products.Domain/Models/Product.cs
+++ source/Sopheon.CloudNative.Products/Sopheon.CloudNative.Products.Domain/Models/Product.cs
@@ -63,32 +63,6 @@ namespace Sopheon.CloudNative.Products.Domain
         public string Description { get; set; }
     }
 
-    public class ProductItem : IRankedEntity, IAttributeContainer
-    {
-        public int Id { get; set; }
-
-        public string Name { get; set; }
-
-        public int ProductItemTypeId { get; set; }
-
-        /// <summary>
-        /// Navigation Property
-        /// </summary>
-        public ProductItemType ProductItemType { get; set; }
-
-        public Rank Rank { get; set; }
-
-        public List<Int32AttributeValue> IntAttributeValues { get; set; }
-
-        public List<StringAttributeValue> StringAttributeValues { get; set; }
-
-        public List<DecimalAttributeValue> DecimalAttributeValues { get; set; }
-
-        public List<UtcDateTimeAttributeValue> UtcDateTimeAttributeValues { get; set; }
-
-        public List<MoneyAttributeValue> MoneyAttributeValues { get; set; }
-    }
-
     public class FileAttachment
     {
         public int FileAttachmentId { get; set; }
---
+++ source/Sopheon.CloudNative.Products/Sopheon.CloudNative.Products.Domain/Models/ProductItem.cs
@@ -0,0 +1,30 @@
+﻿#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
+namespace Sopheon.CloudNative.Products.Domain
+{
+   public class ProductItem : IRankedEntity, IAttributeContainer
+   {
+      public int Id { get; set; }
+
+      public string Name { get; set; }
+
+      public int ProductItemTypeId { get; set; }
+
+      /// <summary>
+      /// Navigation Property
+      /// </summary>
+      public ProductItemType ProductItemType { get; set; }
+
+      public Rank Rank { get; set; }
+
+      public List<Int32AttributeValue> IntAttributeValues { get; set; }
+
+      public List<StringAttributeValue> StringAttributeValues { get; set; }
+
+      public List<DecimalAttributeValue> DecimalAttributeValues { get; set; }
+
+      public List<UtcDateTimeAttributeValue> UtcDateTimeAttributeValues { get; set; }
+
+      public List<MoneyAttributeValue> MoneyAttributeValues { get; set; }
+   }
+}
