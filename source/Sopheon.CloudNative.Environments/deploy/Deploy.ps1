8db81b84696fd1f8012305c4f610a1fc79ec5bc1
Cal Hueber <Cal.Hueber@sopheon.com>
2021-12-16 08:50:26 -0700

    added firewall rules and updated logging

--- source/Sopheon.CloudNative.Environments/deploy/Deploy.ps1
+++ source/Sopheon.CloudNative.Environments/deploy/Deploy.ps1
@@ -16,17 +16,29 @@ $SqlAdminEnigma = (az keyvault secret show --vault-name $azureKeyVault --name "S
 
 #region Sql Script Run
 Write-Host "Running Sql Server Script for Environment Management";
-az sql server firewall-rule create --resource-group $ResourceGroup --server $ResourceGroup.ToLower() --name DeployMachine --start-ip-address 50.200.9.230 --end-ip-address 50.200.9.230;
-Write-Host "Firewall Rule created";
-Write-Host "Running EF Sql Script on Sql Server Database: $($ResourceGroup)...";
+az sql server firewall-rule create --resource-group $ResourceGroup --server $EnvironmentServerName --name DeployMachine --start-ip-address 50.200.9.230 --end-ip-address 50.200.9.230;
+az sql server firewall-rule create --resource-group $ResourceGroup --server $TenantEnvironmentServerName --name DeployMachine --start-ip-address 50.200.9.230 --end-ip-address 50.200.9.230;
+
+Write-Host "Firewall Rules created";
+
+Write-Host "Setting Job Agent Credentials on $EnvironmentServerName";
 Invoke-Sqlcmd -ServerInstance "$($EnvironmentServerName).database.windows.net" -Database 'master' -UserName "sopheon" -Password $SqlAdminEnigma -InputFile "_StratusEnvironmentManagement\EnvironmentManagement\ElasticJobTarget_CreateCredentials.sql"
+
+Write-Host "Setting Job Agent Credentials on $TenantEnvironmentServerName";
 Invoke-Sqlcmd -ServerInstance "$($TenantEnvironmentServerName).database.windows.net" -Database 'master' -UserName "sopheon" -Password $SqlAdminEnigma -InputFile "_StratusEnvironmentManagement\EnvironmentManagement\ElasticJobTarget_CreateCredentials.sql"
+
+Write-Host "Running EF Sql Script on Sql Server Database: $($EnvironmentServerName)...";
 Invoke-Sqlcmd -ServerInstance "$($EnvironmentServerName).database.windows.net" -Database "EnvironmentManagement" -UserName "sopheon" -Password $SqlAdminEnigma -InputFile "_StratusEnvironmentManagement\EnvironmentManagement\scripts.sql" -QueryTimeout 0;
+
+Write-Host "Setting Job User Credentials on $EnvironmentServerName";
 Invoke-Sqlcmd -ServerInstance "$($EnvironmentServerName).database.windows.net" -Database "TenantEnvironmentTemplate" -UserName "sopheon" -Password $SqlAdminEnigma -InputFile "_StratusEnvironmentManagement\EnvironmentManagement\ElasticJobTarget_CreateJobUser.sql";
 
 Write-Host "Complete!";
-Write-Host "Removing Firewall Rule";
-az sql server firewall-rule delete --name DeployMachine --resource-group $ResourceGroup --server $ResourceGroup.ToLower();
+
+Write-Host "Removing Firewall Rules";
+az sql server firewall-rule delete --name DeployMachine --resource-group $ResourceGroup --server $EnvironmentServerName;
+az sql server firewall-rule delete --name DeployMachine --resource-group $ResourceGroup --server $TenantEnvironmentServerName;
+
 
 #upload function app
 az functionapp deployment source config-zip --name "$($ResourceGroup.ToLower())-environment" --resource-group $ResourceGroup --src "_StratusEnvironmentManagement\EnvironmentManagement\EnvironmentManagement.zip";
