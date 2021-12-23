7a2028699056ebd4c1e1885b252522f50384a660
Zach Quinn <ZachQuinn@sopheon.com>
2021-12-23 11:35:21 -0700

    cloud-2497 GET task history with Charles

---
+++ source/Sopheon.CloudNative.Products/Sopheon.CloudNative.Products.Domain/EntitySnapshot.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sopheon.CloudNative.Products.Domain
+{
+
+   /// <summary>
+   /// A snapshot of an entity, and the period that it is valid for
+   /// </summary>
+   /// <typeparam name="T"></typeparam>
+   public class EntitySnapshot<T> where T : class
+   {
+      public T Snapshot { get; set; }
+      public DateTime PeriodStart { get; set; }
+      public DateTime PeriodEnd { get; set; }
+   }
+}
