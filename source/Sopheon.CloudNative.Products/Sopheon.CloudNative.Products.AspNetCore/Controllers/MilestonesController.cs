02144c5536a6c2662e3b4d56fea2d694e9236c8c
Zach Quinn <ZachQuinn@sopheon.com>
2022-01-06 16:13:03 -0700

    cloud-2558 POST milestone

--- source/Sopheon.CloudNative.Products/Sopheon.CloudNative.Products.AspNetCore/Controllers/MilestonesController.cs
+++ source/Sopheon.CloudNative.Products/Sopheon.CloudNative.Products.AspNetCore/Controllers/MilestonesController.cs
@@ -1,7 +1,10 @@
-﻿using AutoMapper;
+﻿using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Sopheon.CloudNative.Products.AspNetCore.Filters;
+using Sopheon.CloudNative.Products.AspNetCore.Models;
 using Sopheon.CloudNative.Products.Domain;
 
 namespace Sopheon.CloudNative.Products.AspNetCore.Controllers
@@ -22,6 +25,23 @@ namespace Sopheon.CloudNative.Products.AspNetCore.Controllers
          _logger = logger;
          _dbContext = dbContext;
          _mapper = mapper;
+      }
+
+
+      [HttpPost]
+      public async Task<IActionResult> PostMilestone(string productKey, [FromBody] MilestoneDto milestoneDto)
+      {
+         Product product = await _dbContext.Products
+             .SingleOrDefaultAsync(p => p.Key == productKey);
+         if (product == null) { return NotFound(); }
+
+         Milestone milestone = _mapper.Map<Milestone>(milestoneDto);
+         milestone.ProductId = product.Id;
+
+         _dbContext.Milestones.Add(milestone);
+         await _dbContext.SaveChangesAsync();
+
+         return Created("TODO-implement Get single Milestone endpoint", _mapper.Map<MilestoneDto>(milestone));
       }
    }
 }
---
+++ source/Sopheon.CloudNative.Products/Sopheon.CloudNative.Products.AspNetCore/Models/MilestoneDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Sopheon.CloudNative.Products.AspNetCore.Models
+{
+   public class MilestoneDto
+   {
+      public int Id { get; set; }
+
+      public string Name { get; set; }
+
+      public string Notes { get; set; }
+
+      public DateTime? Date { get; set; }
+   }
+}
