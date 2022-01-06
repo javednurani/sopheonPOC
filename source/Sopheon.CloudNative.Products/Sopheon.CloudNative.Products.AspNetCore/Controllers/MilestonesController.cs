a617a3e4f4b7fa67d07d14add85214b5e54a2403
Zach Quinn <ZachQuinn@sopheon.com>
2022-01-06 15:29:43 -0700

    cloud-2557 add MilestoneController

---
+++ source/Sopheon.CloudNative.Products/Sopheon.CloudNative.Products.AspNetCore/Controllers/MilestonesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Sopheon.CloudNative.Products.AspNetCore.Filters;
+using Sopheon.CloudNative.Products.AspNetCore.Models;
+using Sopheon.CloudNative.Products.Domain;
+using Task = Sopheon.CloudNative.Products.Domain.Task;
+
+namespace Sopheon.CloudNative.Products.AspNetCore.Controllers
+{
+   [TypeFilter(typeof(GeneralExceptionFilter))]
+
+   [Route("Environments/{EnvironmentId}/Products/{productKey}/[controller]")]
+   public class MilestonesController : EnvironmentScopedControllerBase
+   {
+      private readonly ILogger<TasksController> _logger;
+      private readonly ProductManagementContext _dbContext;
+      private readonly IMapper _mapper;
+
+      public MilestonesController(ILogger<TasksController> logger,
+         ProductManagementContext dbContext,
+         IMapper mapper)
+      {
+         _logger = logger;
+         _dbContext = dbContext;
+         _mapper = mapper;
+      }
+   }
+}
