b826ddb0c37d27b2978f035507cb572121cfe02a
Zach Quinn <ZachQuinn@sopheon.com>
2022-01-06 15:32:05 -0700

    cloud-2557 update MilestonesController

--- source/Sopheon.CloudNative.Products/Sopheon.CloudNative.Products.AspNetCore/Controllers/MilestonesController.cs
+++ source/Sopheon.CloudNative.Products/Sopheon.CloudNative.Products.AspNetCore/Controllers/MilestonesController.cs
@@ -1,16 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using AutoMapper;
-using Microsoft.AspNetCore.JsonPatch;
+﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Sopheon.CloudNative.Products.AspNetCore.Filters;
-using Sopheon.CloudNative.Products.AspNetCore.Models;
 using Sopheon.CloudNative.Products.Domain;
-using Task = Sopheon.CloudNative.Products.Domain.Task;
 
 namespace Sopheon.CloudNative.Products.AspNetCore.Controllers
 {
@@ -19,11 +11,11 @@ namespace Sopheon.CloudNative.Products.AspNetCore.Controllers
    [Route("Environments/{EnvironmentId}/Products/{productKey}/[controller]")]
    public class MilestonesController : EnvironmentScopedControllerBase
    {
-      private readonly ILogger<TasksController> _logger;
+      private readonly ILogger<MilestonesController> _logger;
       private readonly ProductManagementContext _dbContext;
       private readonly IMapper _mapper;
 
-      public MilestonesController(ILogger<TasksController> logger,
+      public MilestonesController(ILogger<MilestonesController> logger,
          ProductManagementContext dbContext,
          IMapper mapper)
       {
