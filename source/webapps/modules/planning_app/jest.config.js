bee00b872724bfc4b61be34a6659f045f85aec87
Jeff Sankeu <jeff.sankeu@sopheon.com>
2022-01-07 11:07:59 -0700

    Code review changes. Fixing planning app folders.

--- source/webapps/modules/planning_app/jest.config.js
+++ source/webapps/modules/planning_app/jest.config.js
@@ -1,8 +1,19 @@
 module.exports = {
   collectCoverageFrom: ['src/**/*.{ts,tsx,js}'],
+  globals: {
+    crypto: require('crypto'),
+  },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'scss'],
-  modulePathIgnorePatterns: ['<rootDir>/.tmp/', '<rootDir>/build/', '<rootDir>/dist/'],
-  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect', '<rootDir>/build/test-setup.ts'],
+  moduleNameMapper: {
+    //'^.+\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '\\.(jpg|jpeg|gif|svg)$': 'identity-obj-proxy',
+    '\\.png$': '<rootDir>/src/__mocks__/fileMock.js',
+  },
+  modulePathIgnorePatterns: ['<rootDir>/.tmp/', '<rootDir>/build/', '<rootDir>/dist/', '<rootDir>/deploy/', '<rootDir>/webpack/'],
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect', '<rootDir>/webpack/test-setup.ts'],
   snapshotSerializers: ['enzyme-to-json/serializer'],
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(tsx?|scss)$',
+  // transform: {
+  //   '^.+\\.tsx?$': 'ts-jest'
+  // }
 };
